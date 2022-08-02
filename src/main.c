30823c45a7232dbd516ec09c3203128e02047e24
Lens <LensPlaysGames@gmail.com>
2022-08-02 11:21:39 -0700

    Progress made on-stream on the 2nd of August
    Also include the example code that the README claims is included :^)

--- src/main.c
+++ src/main.c
@@ -109,23 +109,73 @@ void print_error(Error err) {
 const char *whitespace = " \r\n";
 const char *delimiters = " \r\n,():";
 
+typedef struct Token {
+  char *beginning;
+  char *end;
+  struct Token *next;
+} Token;
+
+Token *token_create() {
+  Token *token = malloc(sizeof(Token));
+  assert(token && "Could not allocate memory for token");
+  memset(token, 0, sizeof(Token));
+  return token;
+}
+
+void token_free(Token *root) {
+  while (root) {
+    Token *token_to_free = root;
+    root = root->next;
+    free(token_to_free);
+  }
+}
+
+void print_token(Token t) {
+  printf("%.*s", t.end - t.beginning, t.beginning);
+}
+
+void print_tokens(Token *root) {
+  size_t count = 1;
+  while (root) {
+    // FIXME: Remove this limit.
+    if (count > 10000) { break; }
+    printf("Token %zu: ", count);
+    if (root->beginning && root->end) {
+      printf("%.*s", root->end - root->beginning, root->beginning);
+    }
+    putchar('\n');
+    root = root->next;
+    count++;
+  }
+}
+
 /// Lex the next token from SOURCE, and point to it with BEG and END.
-Error lex(char *source, char **beg, char **end) {
+Error lex(char *source, Token *token) {
   Error err = ok;
-  if (!source || !beg || !end) {
+  if (!source || !token) {
     ERROR_PREP(err, ERROR_ARGUMENTS, "Can not lex empty source.");
     return err;
   }
-  *beg = source;
-  *beg += strspn(*beg, whitespace);
-  *end = *beg;
-  if (**end == '\0') { return err; }
-  *end += strcspn(*beg, delimiters);
-  if (*end == *beg) {
-    *end += 1;
+  token->beginning = source;
+  token->beginning += strspn(token->beginning, whitespace);
+  token->end = token->beginning;
+  if (*(token->end) == '\0') { return err; }
+  token->end += strcspn(token->beginning, delimiters);
+  if (token->end == token->beginning) {
+    token->end += 1;
   }
   return err;
 }
+
+//      Node-
+//     /  |  \
+//    0   1   2
+//   / \
+//  3   4
+//
+// Node
+// `-- 0  ->  1  ->  2
+//     `-- 3  ->  4
 
 // TODO:
 // |-- API to create new node.
@@ -141,18 +191,96 @@ typedef struct Node {
   union NodeValue {
     integer_t integer;
   } value;
-  struct Node **children;
+  // Possible TODO: Parent?
+  struct Node *children;
+  struct Node *next_child;
 } Node;
 
-#define nonep(node) ((node).type == NODE_TYPE_NONE)
+#define nonep(node)    ((node).type == NODE_TYPE_NONE)
 #define integerp(node) ((node).type == NODE_TYPE_INTEGER)
+
+/// @return Boolean-like value; 1 for success, 0 for failure.
+int node_compare(Node *a, Node *b) {
+  if (!a || !b) {
+    if (!a && !b) {
+      return 1;
+    }
+    return 0;
+  }
+  assert(NODE_TYPE_MAX == 3 && "node_compare() must handle all node types");
+  if (a->type != b->type) { return 0; }
+  switch (a->type) {
+  case NODE_TYPE_NONE:
+    if (nonep(*b)) {
+      return 1;
+    }
+    return 0;
+    break;
+  case NODE_TYPE_INTEGER:
+    if (a->value.integer == b->value.integer) {
+      return 1;
+    }
+    return 0;
+    break;
+  case NODE_TYPE_PROGRAM:
+    // TODO: Compare two programs.
+    printf("TODO: Compare two programs.\n");
+    break;
+  }
+  return 0;
+}
+
+void print_node(Node *node, size_t indent_level) {
+  if (!node) { return; }
+
+  // Print indent.
+  for (size_t i = 0; i < indent_level; ++i) {
+    putchar(' ');
+  }
+  // Print type + value.
+  assert(NODE_TYPE_MAX == 3 && "print_node() must handle all node types");
+  switch (node->type) {
+  default:
+    printf("UNKNOWN");
+  case NODE_TYPE_NONE:
+    printf("NONE");
+    break;
+  case NODE_TYPE_INTEGER:
+    printf("INT:%lld", node->value.integer);
+    break;
+  case NODE_TYPE_PROGRAM:
+    printf("PROGRAM");
+    break;
+  }
+  putchar('\n');
+  // Print children.
+  Node *child = node->children;
+  while (child) {
+    print_node(child, indent_level + 4);
+    child = child->next_child;
+  }
+}
+
+// TODO: Make more efficient! m_n_t_r_a on Twitch suggests keeping track
+// of allocated pointers and then freeing all in one go.
+void node_free(Node *root) {
+  if (!root) { return; }
+  Node *child = root->children;
+  Node *next_child = NULL;
+  while (child) {
+    next_child = child->next_child;
+    node_free(child);
+    child = next_child;
+  }
+  free(root);
+}
 
 // TODO:
 // |-- API to create new Binding.
 // `-- API to add Binding to environment.
 typedef struct Binding {
-  char *id;
-  Node *value;
+  Node id;
+  Node value;
   struct Binding *next;
 } Binding;
 
@@ -162,18 +290,122 @@ typedef struct Environment {
   Binding *bind;
 } Environment;
 
-void environment_set() {
+Environment *environment_create(Environment *parent) {
+  Environment *env = malloc(sizeof(Environment));
+  assert(env && "Could not allocate memory for new environment");
+  env->parent = parent;
+  env->bind = NULL;
+  return env;
+}
 
+void environment_set(Environment env, Node id, Node value) {
+  // Over-write existing value if ID is already bound in environment.
+  Binding *binding_it = env.bind;
+  while (binding_it) {
+    if (node_compare(&binding_it->id, &id)) {
+      binding_it->value = value;
+      return;
+    }
+    binding_it = binding_it->next;
+  }
+  // Create new binding.
+  Binding *binding = malloc(sizeof(Binding));
+  assert(binding && "Could not allocate new binding for environment");
+  binding->id = id;
+  binding->value = value;
+  binding->next = env.bind;
+  env.bind = binding;
+}
+
+Node environment_get(Environment env, Node id) {
+  Binding *binding_it = env.bind;
+  while (binding_it) {
+    if (node_compare(&binding_it->id, &id)) {
+      return binding_it->value;
+    }
+    binding_it = binding_it->next;
+  }
+  Node value;
+  value.type = NODE_TYPE_NONE;
+  value.children = NULL;
+  value.next_child = NULL;
+  value.value.integer = 0;
+  return value;
+}
+
+// @return Boolean-like value; 1 for success, 0 for failure.
+int token_string_equalp(char* string, Token *token) {
+  if (!string || !token) { return 0; }
+  char *beg = token->beginning;
+  while (*string && token->beginning < token->end) {
+    if (*string != *beg) {
+      return 0;
+    }
+    string++;
+    beg++;
+  }
+  return 1;
+}
+
+/// @return Boolean-like value; 1 upon success, 0 for failure.
+int parse_integer(Token *token, Node *node) {
+  if (!token || !node) { return 0; }
+  if (token->end - token->beginning == 1 && *(token->beginning) == '0') {
+    node->type = NODE_TYPE_INTEGER;
+    node->value.integer = 0;
+  } else if ((node->value.integer = strtoll(token->beginning, NULL, 10)) != 0) {
+    node->type = NODE_TYPE_INTEGER;
+  } else { return 0; }
+  return 1;
 }
 
 Error parse_expr(char *source, Node *result) {
-  char *beg = source;
-  char *end = source;
+  size_t token_count = 0;
+  Token current_token;
+  current_token.next = NULL;
+  current_token.beginning  = source;
+  current_token.end        = source;
   Error err = ok;
-  while ((err = lex(end, &beg, &end)).type == ERROR_NONE) {
-    if (end - beg == 0) { break; }
-    printf("lexed: %.*s\n", end - beg, beg);
+
+  Node *root = calloc(1, sizeof(Node));
+  assert(root && "Could not allocate memory for AST Root.");
+  root->type = NODE_TYPE_PROGRAM;
+
+  Node working_node;
+  while ((err = lex(current_token.end, &current_token)).type == ERROR_NONE) {
+    working_node.children = NULL;
+    working_node.next_child = NULL;
+    working_node.type = NODE_TYPE_NONE;
+    working_node.value.integer = 0;
+    size_t token_length = current_token.end - current_token.beginning;
+    if (token_length == 0) { break; }
+    if (parse_integer(&current_token, &working_node)) {
+      // look ahead for binary ops that include integers.
+      Token integer;
+      memcpy(&integer, &current_token, sizeof(Token));
+      err = lex(current_token.end, &current_token);
+      if (err.type != ERROR_NONE) {
+        return err;
+      }
+      // TODO: Check for valid integer operator.
+      // It would be cool to use an operator environment to look up
+      // operators instead of hard-coding them. This would eventually
+      // allow for user-defined operators, or stuff like that!
+    } else {
+      printf("Unrecognized token: ");
+      print_token(current_token);
+      putchar('\n');
+
+      // TODO: Check if valid symbol for variable environment, then
+      // attempt to pattern match variable access, assignment,
+      // declaration, or declaration with initialization.
+
+    }
+    printf("Found node: ");
+    print_node(&working_node, 0);
+    putchar('\n');
   }
+
   return err;
 }
 
