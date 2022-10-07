e8a7526a55924a87af0815a8bba5f7375c73883a
Lens <LensPlaysGames@gmail.com>
2022-10-07 11:43:48 -0700

    The rest of the owl.

---
+++ src/codegen/intermediate_representation.c
@@ -0,0 +1,505 @@
+#include <codegen/intermediate_representation.h>
+
+#include <stdint.h>
+#include <stdlib.h>
+#include <inttypes.h>
+
+void ir_insert
+(CodegenContext *context,
+ IRInstruction *new_instruction
+ )
+{
+  ASSERT(context->block->branch == NULL, "Can not insert into a closed IRBlock.");
+  // [ ] <-> [ ] <-> [NULL]
+  //         [ ] <-> [new] <-> [NULL]
+  if (!context->block->instructions) {
+    context->block->instructions = new_instruction;
+    context->block->last_instruction = new_instruction;
+    return;
+  }
+  context->block->last_instruction->next = new_instruction;
+  new_instruction->previous = context->block->last_instruction;
+  context->block->last_instruction = new_instruction;
+}
+
+#define INSERT(instruction) ir_insert(context, (instruction))
+
+void ir_femit_instruction
+(FILE *file,
+ IRInstruction *instruction
+ )
+{
+  ASSERT(instruction, "Can not insert NULL instruction");
+
+# define ID_FORMAT "%%%zu | ", instruction->id
+  const size_t id_width = 10;
+  size_t id_length = snprintf(NULL, 0, ID_FORMAT);
+  int64_t difference = id_width - id_length;
+  while (difference--) {
+    fputc(' ', file);
+  }
+  fprintf(file, ID_FORMAT);
+# undef ID_FORMAT
+
+  switch (instruction->type) {
+  case IR_IMMEDIATE:
+    fprintf(file, "%"PRId64, instruction->value.immediate);
+    break;
+  case IR_CALL:
+    switch (instruction->value.call.type) {
+    case IR_CALLTYPE_DIRECT:
+      fprintf(file, "%s", instruction->value.call.value.name);
+    case IR_CALLTYPE_INDIRECT:
+      fprintf(file, "%%%zu", instruction->value.call.value.callee->id);
+    default:
+      TODO("Handle %d IRCallType.", instruction->value.call.type);
+      break;
+    }
+    fputc('(', file);
+    IRCallArgument *argument = instruction->value.call.arguments;
+    if (argument) {
+      fprintf(file, "%%%zu", argument->value->id);
+      argument = argument->next;
+    }
+    for (; argument; argument = argument->next) {
+      fprintf(file, ", %%%zu", argument->value->id);
+    }
+    fputc(')', file);
+    break;
+  case IR_RETURN:
+    fprintf(file, "return");
+    break;
+  case IR_ADD:
+    fprintf(file, "add %%%zu, %%%zu",
+            instruction->value.pair.car->id,
+            instruction->value.pair.cdr->id);
+    break;
+  case IR_GLOBAL_STORE:
+    fprintf(file, "g.store %%%zu, %s",
+            instruction->value.global_assignment.new_value->id,
+            instruction->value.global_assignment.name);
+    break;
+  case IR_GLOBAL_ADDRESS:
+    fprintf(file, "g.address %s", instruction->value.name);
+    break;
+  default:
+    TODO("Handle IRType %d\n", instruction->type);
+    break;
+  }
+  fputc('\n', file);
+}
+
+void ir_femit_block
+(FILE *file,
+ IRBlock *block
+ )
+{
+  fprintf(file, "  bb%zu\n", block->id);
+  for (IRInstruction *instruction = block->instructions;
+       instruction;
+       instruction = instruction->next
+       ) {
+    ir_femit_instruction(file, instruction);
+  }
+  ir_femit_instruction(file, block->branch);
+}
+
+void ir_femit_function
+(FILE *file,
+ IRFunction *function
+ )
+{
+  fprintf(file, "f%zu\n", function->id);
+  for (IRBlock *block = function->first;
+       block;
+       block = block->next
+       ) {
+    ir_femit_block(file, block);
+  }
+
+  fprintf(file, "return value: %%%zu\n", function->return_value->id);
+}
+
+void ir_femit
+(FILE *file,
+ CodegenContext *context
+ )
+{
+  for (IRFunction *function = context->all_functions;
+       function;
+       function = function->next
+       ) {
+    ir_femit_function(file, function);
+  }
+  putchar('\n');
+}
+
+void ir_set_ids(CodegenContext *context) {
+  size_t function_id = 0;
+  size_t block_id = 0;
+  size_t instruction_id = 0;
+  for (IRFunction *function = context->function;
+       function;
+       function = function->next
+       ) {
+    for (IRBlock *block = function->first;
+         block;
+         block = block->next
+         ) {
+      for (IRInstruction *instruction = block->instructions;
+           instruction;
+           instruction = instruction->next
+           ) {
+        instruction->id = ++instruction_id;
+      }
+      block->branch->id = ++instruction_id;
+      block->id = ++block_id;
+    }
+    function->id = ++function_id;
+  }
+}
+
+void ir_add_function_call_argument
+(CodegenContext *context,
+ IRInstruction *call,
+ IRInstruction *argument
+ )
+{
+  IRCallArgument *new_argument = calloc(1, sizeof(IRCallArgument));
+  ASSERT(new_argument, "Could not allocate memory for new_argument.");
+
+  IRCallArgument *arguments = call->value.call.arguments;
+  if (!arguments) {
+    arguments = new_argument;
+  } else {
+    for (; arguments->next; arguments = arguments->next);
+    arguments->next = new_argument;
+  }
+}
+
+IRBlock *ir_block_create() {
+  IRBlock *block = calloc(1, sizeof(IRBlock));
+  ASSERT(block, "Could not allocate memory for first IRBlock of new IRFunction.");
+  return block;
+}
+
+void ir_phi_argument
+(IRInstruction *phi,
+ IRBlock *phi_predecessor,
+ IRInstruction *argument
+ )
+{
+  IRPhiArgument *phi_argument = calloc(1, sizeof(IRPhiArgument));
+  phi_argument->block = phi_predecessor;
+  phi_argument->value = argument;
+
+  phi_argument->next = phi->value.phi_argument;
+  phi->value.phi_argument = phi_argument;
+}
+
+IRInstruction *ir_phi(CodegenContext *context) {
+  INSTRUCTION(phi, IR_PHI);
+  return phi;
+}
+
+void ir_block_attach
+(CodegenContext *context,
+ IRBlock *new_block
+ )
+{
+  IRFunction *function = context->function;
+  ASSERT(function->last);
+  function->last->next = new_block;
+  new_block->previous = function->last;
+  function->last = new_block;
+  context->block = new_block;
+}
+
+IRFunction *ir_function_create() {
+  IRFunction *function = calloc(1, sizeof(IRFunction));
+  ASSERT(function, "Could not allocate memory for new IRFunction.");
+  return function;
+}
+
+IRFunction *ir_function(CodegenContext *context) {
+  IRFunction *function = ir_function_create();
+  // A function *must* contain at least one block, so we start new
+  // functions out with an empty block.
+  IRBlock *block = ir_block_create();
+
+  function->next = context->function;
+
+  function->first = block;
+  function->last = block;
+
+  context->function = function;
+  context->block = block;
+
+  if (!context->all_functions) {
+    context->all_functions = function;
+  }
+
+  return function;
+}
+
+IRInstruction *ir_immediate
+(CodegenContext *context,
+ int64_t immediate
+ )
+{
+  INSTRUCTION(imm, IR_IMMEDIATE);
+  imm->value.immediate = immediate;
+  INSERT(imm);
+  return imm;
+}
+
+IRInstruction *ir_load
+(CodegenContext *context,
+ IRInstruction *address
+ )
+{
+  INSTRUCTION(load, IR_LOAD);
+  load->value.reference = address;
+  INSERT(load);
+  return load;
+}
+
+IRInstruction *ir_direct_call
+(CodegenContext *context,
+ char* function_name
+ )
+{
+  INSTRUCTION(call, IR_CALL);
+  call->value.call.type = IR_CALLTYPE_DIRECT;
+  call->value.call.value.name = function_name;
+  INSERT(call);
+  return call;
+}
+
+IRInstruction *ir_indirect_call
+(CodegenContext *context,
+ IRInstruction *function
+ )
+{
+  INSTRUCTION(call, IR_CALL);
+  call->value.call.type = IR_CALLTYPE_INDIRECT;
+  call->value.call.value.callee = function;
+  INSERT(call);
+  return call;
+}
+
+IRInstruction *ir_load_global_address
+(CodegenContext *context,
+ char *name
+ )
+{
+  INSTRUCTION(global_address, IR_GLOBAL_ADDRESS);
+  global_address->value.name = name;
+  INSERT(global_address);
+  return global_address;
+}
+
+IRInstruction *ir_load_local_address
+(CodegenContext *context,
+ IRInstruction *local
+ )
+{
+  INSTRUCTION(local_address, IR_LOCAL_ADDRESS);
+  local_address->value.reference = local;
+  INSERT(local_address);
+  return local_address;
+}
+
+IRInstruction *ir_load_global
+(CodegenContext *context,
+ char *name
+ )
+{
+  INSTRUCTION(global_load, IR_GLOBAL_LOAD);
+  global_load->value.name = name;
+  INSERT(global_load);
+  return global_load;
+}
+
+IRInstruction *ir_load_local
+(CodegenContext *context,
+ IRInstruction *local
+ )
+{
+  INSTRUCTION(local_load, IR_LOCAL_LOAD);
+  local_load->value.reference = local;
+  INSERT(local_load);
+  return local_load;
+}
+
+IRInstruction *ir_store_global
+(CodegenContext *context,
+ IRInstruction *source,
+ char *name
+ )
+{
+  INSTRUCTION(global_store, IR_GLOBAL_STORE);
+  global_store->value.global_assignment.new_value = source;
+  global_store->value.global_assignment.name = name;
+  INSERT(global_store);
+  return global_store;
+}
+
+IRInstruction *ir_store_local
+(CodegenContext *context,
+ IRInstruction *source,
+ IRInstruction *local
+ )
+{
+  INSTRUCTION(local_store, IR_LOCAL_STORE);
+  local_store->value.pair.car = local;
+  local_store->value.pair.cdr = source;
+  INSERT(local_store);
+  return local_store;
+}
+
+IRInstruction *ir_store
+(CodegenContext *context,
+ IRInstruction *data,
+ IRInstruction *address
+ )
+{
+  TODO();
+}
+
+IRInstruction *ir_branch_conditional
+(CodegenContext *context,
+ IRInstruction *condition,
+ IRBlock *then_block,
+ IRBlock *otherwise_block
+ )
+{
+  INSTRUCTION(branch, IR_BRANCH_CONDITIONAL);
+  branch->value.conditional_branch.condition = condition;
+  branch->value.conditional_branch.true_branch = then_block;
+  branch->value.conditional_branch.false_branch = otherwise_block;
+  context->block->branch = branch;
+  return branch;
+}
+
+IRInstruction *ir_branch
+(CodegenContext *context,
+ IRBlock *destination
+ )
+{
+  INSTRUCTION(branch, IR_BRANCH);
+  branch->value.block = destination;
+  context->block->branch = branch;
+  return branch;
+}
+
+IRInstruction *ir_return(CodegenContext *context) {
+  INSTRUCTION(branch, IR_RETURN);
+  context->block->branch = branch;
+  return branch;
+}
+
+IRInstruction *ir_copy
+(CodegenContext *context,
+ IRInstruction *source
+ )
+{
+  TODO();
+}
+
+IRInstruction *ir_comparison
+(CodegenContext *context,
+ enum ComparisonType type,
+ IRInstruction *lhs,
+ IRInstruction *rhs
+ )
+{
+  INSTRUCTION(cc, IR_COMPARISON);
+  cc->value.comparison.type = type;
+  cc->value.comparison.pair.car = lhs;
+  cc->value.comparison.pair.cdr = rhs;
+  INSERT(cc);
+  return cc;
+}
+
+IRInstruction *ir_add
+(CodegenContext *context,
+ IRInstruction *lhs,
+ IRInstruction *rhs
+ )
+{
+  INSTRUCTION(add, IR_ADD);
+  add->value.pair.car = lhs;
+  add->value.pair.cdr = rhs;
+  INSERT(add);
+  return add;
+}
+
+IRInstruction *ir_subtract
+(CodegenContext *context,
+ IRInstruction *lhs,
+ IRInstruction *rhs
+ )
+{
+  INSTRUCTION(sub, IR_SUBTRACT);
+  sub->value.pair.car = lhs;
+  sub->value.pair.cdr = rhs;
+  INSERT(sub);
+  return sub;
+}
+
+IRInstruction *ir_multiply
+(CodegenContext *context,
+ IRInstruction *lhs,
+ IRInstruction *rhs
+ )
+{
+  TODO();
+}
+
+IRInstruction *ir_divide
+(CodegenContext *context,
+ IRInstruction *lhs,
+ IRInstruction *rhs
+ )
+{
+  TODO();
+}
+
+IRInstruction *ir_modulo
+(CodegenContext *context,
+ IRInstruction *lhs,
+ IRInstruction *rhs
+ )
+{
+  TODO();
+}
+
+IRInstruction *ir_shift_left
+(CodegenContext *context,
+ IRInstruction *lhs,
+ IRInstruction *rhs
+ )
+{
+  TODO();
+}
+
+IRInstruction *ir_shift_right_arithmetic
+(CodegenContext *context,
+ IRInstruction *lhs,
+ IRInstruction *rhs
+ )
+{
+  TODO();
+}
+
+IRInstruction *ir_stack_allocate
+(CodegenContext *context,
+ int64_t size
+ )
+{
+  TODO();
+}
+
+#undef INSERT
+
---
+++ src/codegen/intermediate_representation.h
@@ -0,0 +1,305 @@
+#ifndef INTERMEDIATE_REPRESENTATION_H
+#define INTERMEDIATE_REPRESENTATION_H
+
+#include <codegen.h>
+#include <codegen/codegen_forward.h>
+#include <inttypes.h>
+
+#define INSTRUCTION(name, given_type)                       \
+  IRInstruction *(name) = calloc(1, sizeof(IRInstruction)); \
+  ASSERT((name), "Could not allocate new IRInstruction.");  \
+  (name)->type = (given_type);
+
+typedef enum IRType {
+  IR_IMMEDIATE,
+  IR_CALL,
+  IR_RETURN,
+  IR_LOAD,
+  IR_BRANCH,
+  IR_BRANCH_CONDITIONAL,
+  IR_PHI,
+
+  IR_ADD,
+  IR_SUBTRACT,
+
+  IR_LOCAL_LOAD,
+  IR_LOCAL_STORE,
+  IR_LOCAL_ADDRESS,
+
+  IR_GLOBAL_LOAD,
+  IR_GLOBAL_STORE,
+  IR_GLOBAL_ADDRESS,
+
+  IR_COMPARISON,
+  IR_COUNT
+} IRType;
+
+typedef struct IRPhiArgument {
+  /// The value of the argument itself.
+  IRInstruction *value;
+  /// Stores the predecessor to the Phi node in the direction of the
+  /// argument assignment.
+  ///    [a]
+  ///  [t] [o]
+  ///    \ [b]
+  ///    [j]
+  /// For example, if arg->value->block == o, then arg->block == b.
+  IRBlock *block;
+  // A linked list of arguments.
+  struct IRPhiArgument *next;
+} IRPhiArgument;
+
+typedef struct IRPair {
+  IRInstruction *car;
+  IRInstruction *cdr;
+} IRPair;
+
+typedef struct IRCallArgument {
+  IRInstruction *value;
+  struct IRCallArgument *next;
+} IRCallArgument;
+
+typedef enum IRCallType {
+  IR_CALLTYPE_DIRECT,
+  IR_CALLTYPE_INDIRECT,
+  IR_CALLTYPE_COUNT
+} IRCallType;
+
+typedef union IRCallValue {
+  char *name;
+  IRInstruction *callee;
+} IRCallValue;
+
+typedef struct IRCall {
+  IRCallType type;
+  IRCallValue value;
+  IRCallArgument *arguments;
+} IRCall;
+
+typedef struct IRBranchConditional {
+  IRInstruction *condition;
+  IRBlock *true_branch;
+  IRBlock *false_branch;
+} IRBranchConditional;
+
+typedef struct IRComparison {
+  enum ComparisonType type;
+  IRPair pair;
+} IRComparison;
+
+typedef struct IRGlobalAssignment {
+  IRInstruction *new_value;
+  char *name;
+} IRGlobalAssignment;
+
+typedef union IRValue {
+  IRBlock *block;
+  IRInstruction *reference;
+  int64_t immediate;
+  IRCall call;
+  IRPhiArgument *phi_argument;
+  IRBranchConditional conditional_branch;
+  IRPair pair;
+  IRComparison comparison;
+  char *name;
+  IRGlobalAssignment global_assignment;
+} IRValue;
+
+typedef struct IRInstruction {
+  int type;
+  IRValue value;
+
+  /// A unique identifier (mainly for debug purposes).
+  size_t id;
+
+  // Register allocation.
+  size_t index;
+
+  // Doubly linked list.
+  struct IRInstruction *previous;
+  struct IRInstruction *next;
+} IRInstruction;
+
+typedef struct IRBlockPredecessor {
+  IRBlock *block;
+  struct IRBlockPredecessor *next;
+} IRBlockPredecessor;
+
+/// A block is a list of instructions that have control flow enter at
+/// the beginning and leave at the end.
+typedef struct IRBlock {
+  IRInstruction *instructions;
+  IRInstruction *last_instruction;
+
+  IRInstruction *branch;
+
+  IRBlockPredecessor *predecessor;
+
+  // Doubly linked list.
+  struct IRBlock *previous;
+  struct IRBlock *next;
+
+  // Unique ID (among blocks)
+  size_t id;
+} IRBlock;
+
+typedef struct IRFunction {
+  IRBlock *first;
+  IRBlock *last;
+
+  IRInstruction *return_value;
+
+  // Linked list.
+  struct IRFunction *next;
+
+  // Unique ID (among functions)
+  size_t id;
+} IRFunction;
+
+void ir_set_ids(CodegenContext *context);
+
+void ir_femit_instruction
+(FILE *file,
+ IRInstruction *instruction);
+void ir_femit_block
+(FILE *file,
+ IRBlock *block);
+void ir_femit_function
+(FILE *file,
+ IRFunction *function);
+void ir_femit
+(FILE *file,
+ CodegenContext *context);
+
+void ir_add_function_call_argument
+(CodegenContext *context,
+ IRInstruction *call,
+ IRInstruction *argument);
+
+IRBlock *ir_block_create();
+
+void ir_block_attach
+(CodegenContext *context,
+ IRBlock *new_block);
+
+IRFunction *ir_function_create();
+IRFunction *ir_function(CodegenContext *context);
+
+void ir_insert
+(CodegenContext *context,
+ IRInstruction *new_instruction);
+
+void ir_phi_argument
+(IRInstruction *phi,
+ IRBlock *phi_predecessor,
+ IRInstruction *argument);
+
+IRInstruction *ir_phi
+(CodegenContext *context);
+
+IRInstruction *ir_direct_call
+(CodegenContext *context,
+ char* function_name);
+
+IRInstruction *ir_indirect_call
+(CodegenContext *context,
+ IRInstruction *function);
+
+IRInstruction *ir_load_global_address
+(CodegenContext *context,
+ char *name);
+
+IRInstruction *ir_immediate
+(CodegenContext *context,
+ int64_t immediate);
+
+IRInstruction *ir_load
+(CodegenContext *context,
+ IRInstruction *address);
+
+IRInstruction *ir_load_local_address
+(CodegenContext *context,
+ IRInstruction *local);
+
+IRInstruction *ir_load_global
+(CodegenContext *context,
+ char *name);
+
+IRInstruction *ir_load_local
+(CodegenContext *context,
+ IRInstruction *local);
+
+IRInstruction *ir_store_global
+(CodegenContext *context,
+ IRInstruction *source,
+ char *name);
+
+IRInstruction *ir_store_local
+(CodegenContext *context,
+ IRInstruction *source,
+ IRInstruction *local);
+
+IRInstruction *ir_store
+(CodegenContext *context,
+ IRInstruction *data,
+ IRInstruction *address);
+
+IRInstruction *ir_branch_conditional
+(CodegenContext *context,
+ IRInstruction *condition,
+ IRBlock *then_block,
+ IRBlock *otherwise_block);
+
+IRInstruction *ir_branch
+(CodegenContext *context,
+ IRBlock *destination);
+
+IRInstruction *ir_return
+(CodegenContext *context);
+
+IRInstruction *ir_comparison
+(CodegenContext *context,
+ enum ComparisonType type,
+ IRInstruction *lhs,
+ IRInstruction *rhs);
+
+IRInstruction *ir_add
+(CodegenContext *context,
+ IRInstruction *lhs,
+ IRInstruction *rhs);
+
+IRInstruction *ir_subtract
+(CodegenContext *context,
+ IRInstruction *lhs,
+ IRInstruction *rhs);
+
+IRInstruction *ir_multiply
+(CodegenContext *context,
+ IRInstruction *lhs,
+ IRInstruction *rhs);
+
+IRInstruction *ir_divide
+(CodegenContext *context,
+ IRInstruction *lhs,
+ IRInstruction *rhs);
+
+IRInstruction *ir_modulo
+(CodegenContext *context,
+ IRInstruction *lhs,
+ IRInstruction *rhs);
+
+IRInstruction *ir_shift_left
+(CodegenContext *context,
+ IRInstruction *lhs,
+ IRInstruction *rhs);
+
+IRInstruction *ir_shift_right_arithmetic
+(CodegenContext *context,
+ IRInstruction *lhs,
+ IRInstruction *rhs);
+
+IRInstruction *ir_stack_allocate
+(CodegenContext *context,
+ int64_t size);
+
+#endif /* INTERMEDIATE_REPRESENTATION_H */
