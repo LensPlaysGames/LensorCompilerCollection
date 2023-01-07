b539537d158cedd4aae9f41a7f10ba98d1e59ab6
Sirraide <aeternalmail@gmail.com>
2023-01-07 22:35:04 +0100

    [Format] Finish plugin to typecheck format strings

--- fmt-check-plugin/fmt-check-plugin.cc
+++ fmt-check-plugin/fmt-check-plugin.cc
@@ -17,21 +17,332 @@ struct FormatCheckCallback : public match::MatchFinder::MatchCallback {
   explicit FormatCheckCallback(clang::CompilerInstance &ci_) : ci(ci_) {}
 
   void run(const match::MatchFinder::MatchResult &result) override {
-    /*auto call = result.Nodes.getNodeAs<clang::CallExpr>("call");
-    call->dump();*/
+    auto call = result.Nodes.getNodeAs<clang::CallExpr>("call");
+    auto& diags = ci.getDiagnostics();
 
-    /*/// The first argument is the format string.
-    auto format = call->getArg(0);
+    /// The first argument is the format string.
+    auto format = call->getArg(0)->IgnoreImpCasts();
+
+    /// If the argument is a [const] char* rather than a string literal,
+    /// issue a warning.
+    if (format->getType()->isPointerType() && format->getType()->getPointeeType()->isCharType()) {
+      diags.Report(format->getExprLoc(),
+        diags.getCustomDiagID(clang::DiagnosticsEngine::Warning,
+          "Format string is not a constant expression"));
+      return;
+    }
+
+    /// Ignore arguments that are not string literals.
+    if (!format->getType()->isArrayType() ||
+        !format->getType()->getArrayElementTypeNoTypeQual()->isCharType()) {
+      return;
+    }
+
+    /// If this is somehow not a string literal, ignore it.
+    auto lit = clang::dyn_cast<clang::StringLiteral>(format);
+    if (!lit) return;
 
     /// Get the value of the format string.
-    auto format_str = clang::Lexer::getSourceText(
-        clang::CharSourceRange::getTokenRange(format->getSourceRange()),
-        ci.getSourceManager(),
-        ci.getLangOpts()
-    );
-
-    std::cerr << "STRING: " <<  format_str.data() << "\n";*/
+    auto fmt = lit->getString();
+    typecheck_format_string(fmt, call);
   };
+
+  /// Report an invalid format arg.
+  void report(const clang::Expr* arg, std::string_view specifier) {
+    auto& diags = ci.getDiagnostics();
+
+    /// Pretty print the argument name.
+    std::string name = "`" + arg->getType().getAsString() + "`";
+    if (arg->getType()->isTypedefNameType()) {
+      name += " [aka `";
+      name += arg->getType().getCanonicalType().getAsString();
+      name += "`]";
+    }
+
+    /// Report the error.
+    diags.Report(arg->getExprLoc(),
+      diags.getCustomDiagID(clang::DiagnosticsEngine::Error,
+        "Invalid argument type %0 for format specifier '%1'"))
+          << std::vector{arg->getSourceRange()} << name << specifier;
+  }
+
+  /// Typecheck the format string.
+  ///
+  /// Supported format specifiers:
+  ///   - %c: char
+  ///   - %s: const char *, char *
+  ///   - %S: span, string
+  ///   - %C: A colour string. Only printed if colours are enabled.
+  ///   - %d: int
+  ///   - %i: int
+  ///   - %u: unsigned
+  ///   - %D: int64_t
+  ///   - %I: int64_t
+  ///   - %U: uint64_t
+  ///   - %Z: size_t
+  ///   - %zu: size_t
+  ///   - %p: void *
+  ///   - %b: bool
+  ///   - %T: Type *
+  ///   - %F: Another format string + va_list. This format spec therefore takes *two* arguments.
+  ///   - %%: A '%' character.
+  ///   - %m: Reset colours if colours are enabled.
+  ///   - %X, where X is ESCAPE: A literal escape character if you need that for some ungodly reason.
+  ///   - %XX, where X is in [0-9]: "\033[XXm" if colours are enabled, and "" otherwise.
+  ///   - %BXX, where X is in [0-9]: "\033[1;XXm" if colours are enabled, and "" otherwise.
+  void typecheck_format_string(std::string_view fmt, const clang::CallExpr* call) {
+    auto& diags = ci.getDiagnostics();
+    auto& ctx = ci.getASTContext();
+
+    /// The current argument we're looking at.
+    size_t arg_index = 1;
+
+    /// Main formatting loop.
+    for (;;) {
+      /// Find the start of the next format specifier or ANSI escape code.
+      auto pos = fmt.find_first_of("%\033");
+
+      /// No more format specifiers or escape codes. Check for excess arguments.
+      if (pos == std::string_view::npos) {
+        if (arg_index < call->getNumArgs()) {
+          diags.Report(call->getArg(arg_index)->getBeginLoc(),
+            diags.getCustomDiagID(clang::DiagnosticsEngine::Error,
+              "Excess arguments to format string"))
+               << std::vector{clang::SourceRange{
+                    call->getArg(arg_index)->getSourceRange().getBegin(),
+                    call->getArg(call->getNumArgs() - 1)->getSourceRange().getEnd()
+                  }};
+        }
+        return;
+      }
+
+      /// Skip everything up to the next format specifier or escape code.
+      fmt.remove_prefix(pos);
+
+      /// Skip ANSI escape codes.
+      if (fmt[0] == '\033') {
+        /// Find the end of the escape code.
+        auto end = fmt.find('m');
+        bool found = end != std::string_view::npos;
+        if (!found) {
+          diags.Report(call->getBeginLoc(),
+            diags.getCustomDiagID(clang::DiagnosticsEngine::Error,
+              "Unterminated ANSI escape code in format string"));
+        }
+
+        /// Move on to the next format specifier or just skip the \033
+        /// if we couldn’t find a matching 'm'.
+        fmt.remove_prefix(found ? end + 1 : 1);
+        continue;
+      }
+
+      /// Check if we still have enough arguments and extract the next argument.
+#define GET_ARG()                                              \
+  if (arg_index >= call->getNumArgs()) {                       \
+    diags.Report(call->getBeginLoc(),                          \
+      diags.getCustomDiagID(clang::DiagnosticsEngine::Error,   \
+        "Not enough arguments to format string"));             \
+    return;                                                    \
+  }                                                            \
+  auto arg = call->getArg(arg_index++);                        \
+  auto type = arg->getType()
+
+      /// Yeet '%'.
+      fmt.remove_prefix(1);
+
+      /// Handle the format specifier.
+      bool bold = false;
+      switch (auto spec = fmt[0]; fmt.remove_prefix(1), spec) {
+        case 'c': {
+          GET_ARG();
+          if (!type->isCharType()) report(arg, "%c");
+        } break;
+
+        case 's': {
+          GET_ARG();
+          if (!type->isPointerType() || !type->getPointeeType()->isCharType()) report(arg, "%s");
+        } break;
+
+        case 'S': {
+          /// Using GCC extensions is not a problem since this code is only ever
+          /// going to be compiled w/ clang.
+          __label__ after, report_err;
+          GET_ARG();
+
+          /// This is a rather horrible construct, but it’s preferable to a bunch of
+          /// nested if statements or writing `report(); break` 27 times.
+          goto after;
+        report_err:
+          report(arg, "%S");
+          break;
+        after:
+
+          /// Argument must be a struct containing a pointer to a char and a size_t.
+          /// Make sure it’s a record.
+          if (!type->isStructuralType()) goto report_err;
+
+          /// Make sure it’s a struct.
+          auto struct_type = type->getAsStructureType();
+          auto struct_decl = struct_type->getDecl();
+          if (!struct_decl->isStruct()) goto report_err;
+
+          /// Make sure it has two fields.
+          auto fields = struct_decl->fields();
+          auto count = std::distance(fields.begin(), fields.end());
+          if (count != 2) goto report_err;
+
+          /// Typecheck the fields.
+          auto data_field = fields.begin()->getType();
+          if (!data_field->isPointerType() || !data_field->getPointeeType()->isCharType())
+            goto report_err;
+
+          auto size_field = std::next(fields.begin())->getType();
+          if (!size_field->isUnsignedIntegerType() || ci.getASTContext().getTypeSize(size_field) != sizeof(size_t) * 8)
+            goto report_err;
+        } break;
+
+        case 'C': {
+          GET_ARG();
+          if (!type->isPointerType() || !type->getPointeeType()->isCharType()) report(arg, "%C");
+        } break;
+
+        case 'd':
+        case 'i': {
+          GET_ARG();
+          if (!type->isSignedIntegerType() || ci.getASTContext().getTypeSize(type) != 32)
+            report(arg, std::string{"%"} + spec);
+        } break;
+
+        case 'u': {
+          GET_ARG();
+          if (!type->isUnsignedIntegerType() || ci.getASTContext().getTypeSize(type) != 32)
+            report(arg, std::string{"%"} + spec);
+        } break;
+
+        case 'D':
+        case 'I': {
+          GET_ARG();
+          if (!type->isSignedIntegerType() || ci.getASTContext().getTypeSize(type) != 64)
+            report(arg, std::string{"%"} + spec);
+        } break;
+
+        case 'U': {
+          GET_ARG();
+          if (!type->isUnsignedIntegerType() || ci.getASTContext().getTypeSize(type) != 64)
+            report(arg, std::string{"%"} + spec);
+        } break;
+
+        case 'z': {
+          GET_ARG();
+
+          /// Backwards compatibility with '%zu' because we were using that all over the place.
+          if (fmt[0] != 'u') {
+            diags.Report(arg->getExprLoc(),
+              diags.getCustomDiagID(clang::DiagnosticsEngine::Error,
+                "Invalid format specifier '%0' in format string"))
+                  << std::string{"%z"} + (spec == 0 ? std::string{""} : std::string{spec});
+          }
+
+          fmt.remove_prefix(1);
+          if (!type->isUnsignedIntegerType() || ci.getASTContext().getTypeSize(type) != sizeof(size_t) * 8)
+            report(arg, "%zu");
+        } break;
+
+        case 'Z': {
+          GET_ARG();
+          if (!type->isUnsignedIntegerType() || ci.getASTContext().getTypeSize(type) != sizeof(size_t) * 8)
+            report(arg, "%Z");
+        } break;
+
+        case 'p': {
+          GET_ARG();
+
+          /// Make sure the argument is a pointer.
+          if (!type->isPointerType()) report(arg, "%p");
+        } break;
+
+        case 'b': {
+          GET_ARG();
+          if (!type->isBooleanType()) report(arg, "%b");
+        } break;
+
+        case 'T': {
+          GET_ARG();
+
+          /// Argument must be a pointer to a type whose name is `Type`.
+          if (!type->isPointerType() || type->getPointeeType().getUnqualifiedType().getAsString() != "Type")
+            report(arg, "%T");
+        } break;
+
+        case 'F': {
+          GET_ARG();
+
+          /// The first argument must be a `[const] char *`.
+          if (!type->isPointerType() || !type->getPointeeType()->isCharType())
+            report(arg, "%F");
+
+          /// After that, we need a `va_list` argument.
+          if (arg_index >= call->getNumArgs()) {
+            diags.Report(call->getBeginLoc(),
+              diags.getCustomDiagID(clang::DiagnosticsEngine::Error,
+                "Not enough arguments to format string. Hint: %F requires two arguments "
+                "(a `[const] char*` and a `va_list`)"));
+            return;
+          }
+
+          /// The second argument must be a `va_list`. The absurd amount of conversions below
+          /// are due to the fact that a `va_list` is typically something like `__va_list_tag[1]`,
+          /// but as an argument, this has already been decayed to a pointer, so we need to take
+          /// that into account.
+          auto va_arg = call->getArg(arg_index++);
+          auto va_type = va_arg->getType()->getUnqualifiedDesugaredType();
+          if (va_type->isPointerType() || va_type->isArrayType()) va_type = va_type->getPointeeOrArrayElementType();
+          auto va_list_t = ci.getASTContext().getBuiltinVaListType()->getUnqualifiedDesugaredType();
+          if (va_list_t->isPointerType() || va_list_t->isArrayType()) va_list_t = va_list_t->getPointeeOrArrayElementType();
+          if (va_type != va_list_t) report(va_arg, "%F");
+        } break;
+
+        case '%':
+        case '\033':
+        case 'm':
+          break;
+
+        case 'B':
+          /// Next character must be a digit.
+          bold = true;
+          if (fmt[0] < '0' || fmt[0] > '9') {
+            diags.Report(call->getBeginLoc(),
+              diags.getCustomDiagID(clang::DiagnosticsEngine::Error,
+                "Invalid format specifier '%0' in format string"))
+                  << std::string{"%"} + (spec == 0 ? std::string{""} : std::string{spec});
+            break;
+          }
+          fmt.remove_prefix(1);
+          [[fallthrough]];
+
+        case '0' ... '9':
+          /// Colour codes are always 2 digits.
+          if (fmt[0] < '0' || fmt[0] > '9') {
+            diags.Report(call->getBeginLoc(),
+              diags.getCustomDiagID(clang::DiagnosticsEngine::Error,
+                "Invalid format specifier '%0%1%2%3' in format string"))
+                  << "%" << (bold ? "B" : "") << fmt[-1] << fmt[0];
+            break;
+          }
+
+          /// Ignore this if colours are disabled.
+          fmt.remove_prefix(1);
+          break;
+
+        default:
+          diags.Report(call->getBeginLoc(),
+            diags.getCustomDiagID(clang::DiagnosticsEngine::Error,
+              "Invalid format specifier '%0' in format string"))
+                << std::string{"%"} + (spec == 0 ? std::string{""} : std::string{spec});
+          break;
+      }
+    }
+  }
 };
 
 /// Consumer.
@@ -41,7 +352,24 @@ struct FmtCheckConsumer : public clang::ASTConsumer {
   explicit FmtCheckConsumer(clang::CompilerInstance &CI, clang::ASTContext& ctx2) : CI(CI), ctx(ctx2) {}
 
   void HandleTranslationUnit(clang::ASTContext &Context) override {
-    Context.getTranslationUnitDecl();
+    /// Create a matcher.
+    match::MatchFinder finder;
+
+    /// Create a callback.
+    FormatCheckCallback callback(CI);
+
+    /// Add the callback to the matcher.
+    for (auto &func : format_funcs)
+      finder.addMatcher(
+        match::callExpr(
+          match::callee(
+            match::functionDecl(
+              match::hasName(func)))
+        ).bind("call"),
+      &callback);
+
+    /// Run the matcher.
+    finder.matchAST(Context);
   }
 };
 
@@ -62,5 +390,6 @@ struct FmtCheckPlugin : public clang::PluginASTAction {
     return AddAfterMainAction;
   }
 };
+
 
 static clang::FrontendPluginRegistry::Add<FmtCheckPlugin> X{"fmt-check-plugin", "Format checker"};
