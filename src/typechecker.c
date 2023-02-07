7253339d335cdfa8372f578bcecd3f73b65c3a84
Lens <LensPlaysGames@gmail.com>
2023-02-06 21:24:15 -0800

    [Minor/Sema] Use `vector_back` instead of manual calculation

--- src/typechecker.c
+++ src/typechecker.c
@@ -790,7 +790,7 @@ NODISCARD bool typecheck_expression(AST *ast, Node *expr) {
       Type *ret = expr->type->function.return_type;
       Type *body = expr->function.body->type;
       if (!convertible(ret, body))
-        ERR(expr->function.body->block.children.data[expr->function.body->block.children.size - 1]->source_location,
+        ERR(vector_back(expr->function.body->block.children)->source_location,
             "Type '%T' of function body is not convertible to return type '%T'.",
             body, ret);
     } break;
