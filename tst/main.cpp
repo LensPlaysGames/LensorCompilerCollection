2c3861aa8824c44aa0e878f8be4e37c0e5ed54a7
Sirraide <aeternalmail@gmail.com>
2023-07-10 14:54:00 +0200

    [Tests] Refactor test instantiator

--- tst/main.cpp
+++ tst/main.cpp
@@ -1,33 +1,39 @@
-#include <array>
 #include <algorithm>
+#include <array>
 #include <chrono>
+#include <clopts.hh>
+#include <cstdio>
+#include <cstring>
 #include <filesystem>
+#include <fmt/format.h>
+#include <fmt/std.h>
 #include <fstream>
 #include <iterator>
 #include <random>
 #include <string>
 #include <thread>
-
-#include <cstring>
-#include <cstdio>
+#include <regex>
 
 #ifdef _WIN32
-# define PLATFORM_EXE_SUFFIX "exe"
-# define CALLING_CONVENTION "MSWIN"
+#    define PLATFORM_EXE_SUFFIX "exe"
+#    define CALLING_CONVENTION  "MSWIN"
 #else
-# include <sys/wait.h>
-# define PLATFORM_EXE_SUFFIX ""
-# define CALLING_CONVENTION "LINUX"
+#    include <sys/wait.h>
+#    define PLATFORM_EXE_SUFFIX ""
+#    define CALLING_CONVENTION  "LINUX"
 #endif
 
-std::filesystem::path temppath(std::string_view extension) {
+namespace fs = std::filesystem;
+namespace chr = std::chrono;
+
+fs::path temppath(std::string_view extension) {
     std::mt19937 rd(std::random_device{}());
 
     /// Get the temporary directory.
-    auto tmp_dir = std::filesystem::temp_directory_path();
+    auto tmp_dir = fs::temp_directory_path();
 
     /// Get the current time, pid, and tid.
-    auto now = std::chrono::system_clock::now().time_since_epoch().count();
+    auto now = chr::system_clock::now().time_since_epoch().count();
     auto pid = std::to_string(rd());
     auto tid = std::to_string((uint32_t) std::hash<std::thread::id>{}(std::this_thread::get_id()));
 
@@ -35,13 +41,11 @@ std::filesystem::path temppath(std::string_view extension) {
     /// Do NOT use `char` for this because it’s signed on some systems (including mine),
     /// which completely breaks the modulo operation below... Thanks a lot, C.
     std::array<uint8_t, 8> rand{};
-    std::generate(rand.begin(), rand.end(), [&] () { return rd() % 26 + 'a'; });
+    std::generate(rand.begin(), rand.end(), [&]() { return rd() % 26 + 'a'; });
 
     /// Create a unique path.
-    tmp_dir /= pid; tmp_dir += ".";
-    tmp_dir += tid; tmp_dir += ".";
-    tmp_dir += std::to_string(now); tmp_dir += ".";
-    tmp_dir += std::string_view{(char*) rand.data(), rand.size()};
+    tmp_dir /= pid;
+    tmp_dir += fmt::format(".{}.{}.{}", tid, now, std::string_view{(char *) rand.data(), rand.size()});
     if (not extension.empty()) {
         if (not extension.starts_with('.')) tmp_dir += ".";
         tmp_dir += extension;
@@ -58,7 +62,8 @@ std::filesystem::path temppath(std::string_view extension) {
 template <typename callable>
 struct $$defer_type {
     callable cb;
-    explicit $$defer_type(callable&& _cb) : cb(std::forward<callable>(_cb)) {}
+    explicit $$defer_type(callable&& _cb)
+        : cb(std::forward<callable>(_cb)) {}
     ~$$defer_type() { cb(); }
 };
 
@@ -107,261 +112,251 @@ auto run_command(std::string_view command) -> exit_status {
 
 /// Disallow using system directly for portability reasons.
 #if __GNUG__ || __clang__
-#pragma GCC poison system
+#    pragma GCC poison system
 #endif
 
-int main(int argc, char **argv) {
-    // Expecting signature:
-    // <invocation> `--target` <target> `--test` <test-path> `--intc` <intc-path> `--cc` <c-compiler-path> [ `-O` ]
-    // <target> is passed directly to intc after `-t`
-    if (argc < 9 || argc > 10) return 127;
-    if (strcmp(argv[1], "--target") != 0) return 127;
-    if (strcmp(argv[3], "--test") != 0) return 127;
-    if (strcmp(argv[5], "--intc") != 0) return 127;
-    if (strcmp(argv[7], "--cc") != 0) return 127;
-    if (argc == 10 && strcmp(argv[9], "-O") != 0) return 127;
+namespace detail {
+using namespace command_line_options;
+using options = clopts< // clang-format off
+    option<"--target", "The target to pass to the Intercept compiler", std::string, true>,
+    option<"--test", "The path to the test file", std::string, true>,
+    option<"--intc", "The path to the Intercept compiler", std::string, true>,
+    option<"--cc", "The C compiler to use", std::string, true>,
+    flag<"-O", "Whether to enable optimisations">,
+    help<>
+>; // clang-format on
 
-    bool optimise = false;
-    if (argc == 8) optimise = true;
+}
 
-    std::string intc_target{argv[2]};
-    std::filesystem::path testpath{argv[4]};
-    std::filesystem::path intcpath{argv[6]};
-    std::filesystem::path ccpath{argv[8]};
+#define ASSERT(cond, ...)                     \
+    do {                                      \
+        if (not(cond)) fatal("" __VA_ARGS__); \
+    } while (0)
 
-    if (!std::filesystem::exists(testpath)) {
-        fprintf(stderr, "Sorry, but the test specified at \"%s\" does not exist\n", testpath.string().c_str());
-        return 127;
-    }
-    if (!std::filesystem::exists(intcpath)) {
-        fprintf(stderr, "Sorry, but the intc compiler specified at \"%s\" does not exist\n", intcpath.string().c_str());
-        return 127;
-    }
+/// Check a condition and exit if it isn’t true.
+template <typename... arguments>
+[[noreturn]] void fatal(fmt::format_string<arguments...> fmt, arguments&&...args) {
+    fmt::print(stderr, fmt, std::forward<arguments>(args)...);
+    fmt::print(stderr, "\n");
+    std::exit(127);
+}
+
+/// Get the contents of a file.
+///
+/// This uses an internal function, but I wrote it so I
+/// know how it’s supposed to be used.
+auto map_file(const fs::path& path) -> std::string {
+    namespace cmd = command_line_options;
+    return cmd::detail::map_file<cmd::file_data>(path.string(), [&](std::string&& msg) {
+               fatal("Failed to open output file '{}': {}", path, msg);
+           })
+        .contents;
+}
+
+/// Delete a file if it exists.
+auto delete_file(const fs::path& path) {
+    if (fs::exists(path)) fs::remove(path);
+}
+
+int main(int argc_, char **argv_) {
+    using detail::options;
+    options::parse(argc_, argv_);
+
+    fs::path testpath{*options::get<"--test">()};
+    fs::path intcpath{*options::get<"--intc">()};
+    fs::path ccpath{*options::get<"--cc">()};
+
+    ASSERT(fs::exists(testpath), "Sorry, but the test specified at \"{}\" does not exist", testpath);
+    ASSERT(fs::exists(intcpath), "Sorry, but the intc compiler specified at \"{}\" does not exist", intcpath);
 
     // Parse expected test results
     std::ifstream testfile(testpath);
     std::string line{};
-    if (!std::getline(testfile, line)) {
-        fprintf(stderr, "Sorry, but the test file at \"%s\" appears to be empty\n", testpath.string().c_str());
-        return 127;
-    }
+    ASSERT(std::getline(testfile, line), "Sorry, but the test file at \"{}\" appears to be empty", testpath);
     while (line.starts_with(";; LABELS")) {
-        if (!std::getline(testfile, line)) {
-            fprintf(stderr, "Sorry, but the test file at \"%s\" appears to be malformed (LABELS nonsense)\n", testpath.string().c_str());
-            return 127;
-        }
+        ASSERT(
+            std::getline(testfile, line),
+            "Sorry, but the test file at \"{}\" appears to be malformed (LABELS nonsense)",
+            testpath
+        );
     }
-    if (!line.starts_with(";; ")) {
-        fprintf(stderr,
-                "Sorry, but the test file at \"%s\" appears to be a malformed test.\n"
-                "There must be a \";; \" at the beginning of the first line followed by either\n"
-                "\"ERROR\", \"SKIP\", or an integer status code that is the expected return value.\n",
-                testpath.string().c_str());
-        return 127;
-    }
+
+    ASSERT(
+        line.starts_with(";; "),
+        "Sorry, but the test file at \"{}\" appears to be a malformed test.\n"
+        "There must be a \";; \" at the beginning of the first line followed by either\n"
+        "\"ERROR\", \"SKIP\", or an integer status code that is the expected return value.",
+        testpath
+    );
+
     bool expected_error{false};
     int expected_status{0};
-    if (line.substr(3, 4) == "SKIP") {
-        return 0;
-    } else if (line.substr(3, 5) == "ERROR") {
-        expected_error = true;
-    } else {
-        // I fucking hate exceptions.
-        try {
-            expected_status = std::stoi(line.substr(3));
-        }
-        catch (std::exception e) {
-            fprintf(stderr,
-                    "Sorry, an exception occured while parsing expected return status from first line\n"
-                    "  first line: \"%s\"\n"
-                    "  exception: \"%s\"\n",
-                    line.c_str(),
-                    e.what());
-            return 127;
-        }
+    if (line == ";; SKIP") return 0;
+    if (line == ";; ERROR") expected_error = true;
+    else {
+        char *end;
+        errno = 0;
+        expected_status = (int) std::strtoull(line.c_str() + 3, &end, 10);
+        ASSERT(
+            errno == 0,
+            "Expected exit code must be an integer, but was \"{}\"",
+            std::string_view{line}.substr(3)
+        );
     }
+
     std::string expected_output{};
-    for (; std::getline(testfile, line);) {
-        if (!line.starts_with(";; ")) break;
-        expected_output += line.substr(3) + "\n";
+    while (std::getline(testfile, line)) {
+        if (not line.starts_with(";; ")) break;
+        expected_output += std::string_view{line}.substr(3);
+        expected_output += "\n";
     }
 
     testfile.close();
 
-
-    std::filesystem::path intc_outpath{};
-    if (intc_target.starts_with("asm")) intc_outpath = temppath("s");
-    else intc_outpath = temppath(intc_target == "llvm" ? "ll" : "o");
-
-    std::string intc_invocation{};
-    intc_invocation += intcpath.string();
-    // Set calling convention
-    intc_invocation += " -cc ";
-    intc_invocation += CALLING_CONVENTION;
-    // Set target
-    intc_invocation += " -t ";
-    intc_invocation += intc_target;
-    // Set output file
-    intc_invocation += " -o ";
-    intc_invocation += intc_outpath.string();
-    intc_invocation += " ";
-    if (optimise) intc_invocation += " -O ";
-    // Path to file to compile
-    intc_invocation += testpath.string();
-
-    std::filesystem::path cc_outpath = temppath(PLATFORM_EXE_SUFFIX);
-    std::string cc_invocation{};
-    cc_invocation += ccpath.string();
-    cc_invocation += " -o ";
-    cc_invocation += cc_outpath.string();
-    cc_invocation += " ";
-    cc_invocation += intc_outpath.string();
-
-    std::filesystem::path outpath = temppath("txt");
-    std::string test_invocation = cc_outpath.is_absolute()
-        ? cc_outpath.string()
-        : (std::filesystem::current_path() / cc_outpath).string();
-    test_invocation += " > ";
-    test_invocation += outpath.string();
+    /// Construct Intercept compiler invocation.
+    fs::path intc_outpath{}, intc_logpath = temppath("log");
+    if (options::get<"--target">()->starts_with("asm")) intc_outpath = temppath("s");
+    else intc_outpath = temppath(*options::get<"--target">() == "llvm" ? "ll" : "o");
+    auto intc_invocation = fmt::format(
+        "{} -cc {} -t {} -o {} {}{} > {} 2>&1",
+        intcpath,
+        CALLING_CONVENTION,
+        *options::get<"--target">(),
+        intc_outpath,
+        testpath,
+        options::get<"-O">() ? " -O" : "",
+        intc_logpath
+    );
 
     auto status = run_command(intc_invocation);
-    // Delete generated output file at end of scope.
     defer {
-        if (std::filesystem::exists(intc_outpath))
-            std::filesystem::remove(intc_outpath);
+        delete_file(intc_outpath);
+        delete_file(intc_logpath);
     };
-    // TODO: Error on ICE no matter what. Check output for "Internal Compiler Error".
-    // This means we'd have to redirect and capture intc_invocation output as well.
+
+    /// Check for ICEs.
+    std::string log = map_file(intc_logpath);
+    ASSERT(
+        log.find("Internal Compiler Error") == std::string::npos,
+        "Intercept compiler suffered Internal Compiler Error: {}",
+        log
+    );
+
+    /// If we were expecting an error, check for that.
     if (expected_error) {
-        if (status.success) {
-            fprintf(stderr,
-                    "\nFAILURE: Test returned successful exit code but an error was expected\n"
-                    "  intc_invocation: \"%s\"\n"
-                    "  cc_invocation:   \"%s\"\n"
-                    "  test_invocation: \"%s\"\n",
-                    intc_invocation.c_str(),
-                    cc_invocation.c_str(),
-                    test_invocation.c_str());
-            return 127;
-        }
-        // If status is non-zero (unsucessful) and error was expected, we good.
+        ASSERT(
+            !status.success,
+            "FAILURE: Intercept compiler returned successful exit code but an error was expected\n"
+            "  intc_invocation: \"{}\"",
+            intc_invocation
+        );
+
+        /// If unsuccessful and error was expected, we good.
         return 0;
     }
-    if (not status.success) {
-        fprintf(stderr,
-                "\nFAILURE: intc did not exit successfully\n"
-                "  intc_invocation: \"%s\"\n"
-                "  return status:   %d\n",
-                intc_invocation.c_str(),
-                status.raw_code);
-        return 127;
-    }
 
+    /// Otherwise, the compiler invocation should have succeeded.
+    ASSERT(
+        status.success,
+        "FAILURE: intc did not exit successfully\n"
+        "  intc_invocation: \"{}\"\n"
+        "  return status:     {}\n"
+        "  output:\n{}",
+        intc_invocation,
+        status.raw_code,
+        log
+    );
+
+    /// Construct C compiler invocation.
+    auto cc_outpath = temppath(PLATFORM_EXE_SUFFIX);
+    auto cc_invocation = fmt::format(
+        "{} -o {} {}",
+        ccpath,
+        cc_outpath,
+        intc_outpath
+    );
+
+    /// Run C compiler.
     status = run_command(cc_invocation);
-    defer {
-        if (std::filesystem::exists(cc_outpath))
-            std::filesystem::remove(cc_outpath);
-    };
+    defer { delete_file(cc_outpath); };
+    ASSERT(
+        status.success,
+        "FAILURE: C compiler errored\n"
+        "  intc_invocation: \"{}\"\n"
+        "  cc_invocation:   \"{}\n"
+        "  return status:     {}",
+        intc_invocation,
+        cc_invocation,
+        status.raw_code
+    );
 
-    if (not status.success) {
-        fprintf(stderr,
-                "\nFAILURE: C compiler errored\n"
-                "  intc_invocation: \"%s\"\n"
-                "  cc_invocation:   \"%s\"\n"
-                "  return status:   %d\n",
-                intc_invocation.c_str(),
-                cc_invocation.c_str(),
-                status.raw_code);
-        return 1;
-    }
+    /// Output file for test.
+    fs::path outpath = temppath("txt");
+    auto test_invocation = fmt::format(
+        "{} > {} 2>&1", /// At least this works the same on both Linux and Windows.
+        cc_outpath.is_absolute() ? cc_outpath.string() : (fs::current_path() / cc_outpath).string(),
+        outpath
+    );
 
+    /// Run the test.
     status = run_command(test_invocation);
-    defer {
-        if (std::filesystem::exists(outpath))
-            std::filesystem::remove(outpath);
-    };
+    defer { delete_file(outpath); };
 
 #ifdef __linux__
-    if (not status.exited) {
-        fprintf(stderr,
-                "\nFAILURE: Test was terminated by signal\n"
-                "  intc_invocation: \"%s\"\n"
-                "  cc_invocation:   \"%s\"\n"
-                "  test_invocation: \"%s\"\n"
-                "  signal:          %d\n",
-                intc_invocation.c_str(),
-                cc_invocation.c_str(),
-                test_invocation.c_str(),
-                WTERMSIG(status.code));
-        return 1;
-    }
+    /// Check for signals on Linux.
+    ASSERT(
+        status.exited,
+        "FAILURE: Test was terminated by signal\n"
+        "  intc_invocation: \"{}\"\n"
+        "  cc_invocation:   \"{}\"\n"
+        "  test_invocation: \"{}\"\n"
+        "  signal:            {}",
+        intc_invocation,
+        cc_invocation,
+        test_invocation,
+        WTERMSIG(status.code)
+    );
 #endif
 
-    if (status.code != expected_status) {
-        fprintf(stderr,
-                "\nFAILURE: Test returned unexpected exit code\n"
-                "  intc_invocation: \"%s\"\n"
-                "  cc_invocation:   \"%s\"\n"
-                "  test_invocation: \"%s\"\n"
-                "  return code:     %d\n"
-                "  expected:        %d\n",
-                intc_invocation.c_str(),
-                cc_invocation.c_str(),
-                test_invocation.c_str(),
-                status.code,
-                expected_status);
-        return 1;
-    }
+    /// Check status code.
+    ASSERT(
+        status.code == expected_status,
+        "FAILURE: Test returned unexpected exit code\n"
+        "  intc_invocation: \"{}\"\n"
+        "  cc_invocation:   \"{}\"\n"
+        "  test_invocation: \"{}\"\n"
+        "  return code:       {}\n"
+        "  expected:          {}\n",
+        intc_invocation,
+        cc_invocation,
+        test_invocation,
+        status.code,
+        expected_status
+    );
 
-    std::fstream test_output(outpath);
-    std::string output {
-        std::istreambuf_iterator<char>(test_output),
-        std::istreambuf_iterator<char>()
-    };
-    // Delete test output file
-    test_output.close();
+    /// Get test output.
+    std::string output = map_file(outpath);
 
-    // NOTE: there is some nonsense here having to do with newlines
-    // being automatically added/translated into \r\n from \n, etc.
-    // Basically, expected_output *always* has a newline added, so this should too.
-    if (!output.empty() && !output.ends_with("\n")) output += "\n";
+    /// Convert \r\n to \n.
+    output = std::regex_replace(output, std::regex{"\r\n"}, "\n");
 
-    // Remove all '\r' from both strings, just in case...
-    output.erase(std::remove_if(output.begin(),
-                                output.end(),
-                                [](const char c){ return c == '\r'; }),
-                 output.end());
-    expected_output.erase(std::remove_if(expected_output.begin(),
-                                         expected_output.end(),
-                                         [](const char c){ return c == '\r'; }),
-                          expected_output.end());
+    /// Remove trailing newlines from output and expected output, for consistency.
+    while (output.ends_with("\n")) output.pop_back();
+    while (expected_output.ends_with("\n")) expected_output.pop_back();
 
-    printf("\noutput:\n");
-    for (const char c : output) {
-        printf("%d\n", c);
-    }
-    printf("expected:\n");
-    for (const char c : expected_output) {
-        printf("%d\n", c);
-    }
-
-    if (output != expected_output) {
-        fprintf(stderr,
-                "\nFAILURE: Test generated unexpected output\n"
-                "  intc_invocation: \"%s\"\n"
-                "  cc_invocation:   \"%s\"\n"
-                "  test_invocation: \"%s\"\n"
-                "  output: (until ---)\n"
-                "%s---\n"
-                "  expected:\n"
-                "%s---\n",
-                intc_invocation.c_str(),
-                cc_invocation.c_str(),
-                test_invocation.c_str(),
-                output.c_str(),
-                expected_output.c_str());
-        return 127;
-    }
-
-    return 0;
+    /// Check output.
+    ASSERT(
+        output == expected_output,
+        "FAILURE: Test generated unexpected output\n"
+        "  Output:          {:?}\n"
+        "  Expected:        {:?}\n"
+        "  intc_invocation: \"{}\"\n"
+        "  cc_invocation:   \"{}\"\n"
+        "  test_invocation: \"{}\"",
+        output,
+        expected_output,
+        intc_invocation,
+        cc_invocation,
+        test_invocation
+    );
 }
