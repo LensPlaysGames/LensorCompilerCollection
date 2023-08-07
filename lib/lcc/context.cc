4a1d69e0f9b42392e24f86f4d7278ca2712a8340
nashiora <22553694+nashiora@users.noreply.github.com>
2023-08-07 19:22:14 -0400

    [Utils] Assert file ID is in range

--- lib/lcc/context.cc
+++ lib/lcc/context.cc
@@ -13,6 +13,7 @@ auto lcc::Context::make_file(fs::path name, std::vector<char>&& contents) -> Fil
     /// Create the file.
     auto fptr = new File(*this, std::move(name), std::move(contents));
     fptr->id = u32(owned_files.size());
+    LCC_ASSERT(fptr->id <= std::numeric_limits<u16>::max());
     owned_files.emplace_back(fptr);
     return *fptr;
 }
