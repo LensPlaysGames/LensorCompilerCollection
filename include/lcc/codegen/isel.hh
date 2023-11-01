7903cdfd0499fc41ff9e0da743988dae93110cdc
Lens <lensplaysgames@gmail.com>
2023-11-01 12:35:49 -0700

    [LCC/Codegen/ISel] Begin x86_64 patterns

--- include/lcc/codegen/isel.hh
+++ include/lcc/codegen/isel.hh
@@ -5,7 +5,6 @@
 #include <lcc/codegen/mir.hh>
 
 namespace lcc {
-
 namespace isel {
 
 template<i64 imm = 0>
@@ -27,34 +26,27 @@ struct Inst {
     static constexpr usz opcode = opcode_;
 };
 
-template <typename i, typename o>
+template <typename in, typename out>
 struct Pattern {
-    static void rewrite(MFunction& function);
-};
-
-using ret = Pattern<
-    Inst<usz(Value::Kind::Return)>,
-    Inst<0x420>
-    >;
-
-using ret_imm = Pattern<
-    Inst<usz(Value::Kind::Return), Operand<MOperandImmediate, Immediate<>>>,
-    Inst<0x420, o<1>>
-    >;
-
-template<typename in, typename out>
-void Pattern<in, out>::rewrite(MFunction& function) {
-    for (auto& block : function.blocks()) {
-        for (auto& instruction : block.instructions()) {
-            if (instruction.kind() == in::opcode) {
-                fmt::print("Matching {}!\n", ToString(instruction.kind()));
+    static void rewrite(MFunction& function) {
+        for (auto& block : function.blocks()) {
+            for (auto& instruction : block.instructions()) {
+                if (instruction.kind() == in::opcode) {
+                    fmt::print("Matching {}!\n", ToString(instruction.kind()));
+                }
             }
         }
     }
-}
+};
 
-}
+template<typename... Patterns>
+struct PatternList {
+    static void rewrite(MFunction& function) {
+        (Patterns::rewrite(function), ...);
+    }
+};
 
+} // namespace isel
 } // namespace lcc
 
 #endif /* LCC_CODEGEN_INSTRUCTION_SELECTION_HH */
---
+++ include/lcc/codegen/isel_patterns_x86_64.hh
@@ -0,0 +1,34 @@
+#ifndef LCC_CODEGEN_ISEL_X86_64_PATTERNS_HH
+#define LCC_CODEGEN_ISEL_X86_64_PATTERNS_HH
+
+#include <lcc/codegen/isel.hh>
+
+namespace lcc {
+namespace isel {
+
+enum struct x86_64Opcode : u32 {
+    Poison = u32(lcc::MInst::Kind::ArchStart),
+    Return,
+};
+
+using ret = Pattern<
+    Inst<usz(MInst::Kind::Return)>,
+    Inst<usz(x86_64Opcode::Return)>
+>;
+
+using ret_imm = Pattern<
+    Inst<usz(MInst::Kind::Return), Operand<MOperandImmediate, Immediate<>>>,
+    Inst<usz(x86_64Opcode::Return), o<1>>
+>;
+
+void rewrite_x86_64(MFunction& function) {
+    PatternList<
+        ret,
+        ret_imm
+    >::rewrite(function);
+}
+
+} // namespace isel
+} // namespace lcc
+
+#endif /* LCC_CODEGEN_ISEL_X86_64_PATTERNS_HH */
