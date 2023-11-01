2e33826965e6b47a299f8b02f75d9061a9b1de04
Lens <lensplaysgames@gmail.com>
2023-11-01 14:13:36 -0700

    [Intercept/Sema] Insert returns into void functions
    Void functions don't return a value, but they still need to return!

--- lib/intercept/sema.cc
+++ lib/intercept/sema.cc
@@ -447,18 +447,22 @@ void intc::Sema::AnalyseFunctionBody(FuncDecl* decl) {
     if (not ty->return_type()->is_void()) {
         Expr** last{};
         if (auto block = cast<BlockExpr>(decl->body())) {
-            if (block->children().empty() and not ty->return_type()->is_void()) {
-                // For anything except the top-level function, if there is an expected
-                // return value, there has to be one, otherwise it's an error.
-                if (decl->name() != "main") {
-                    Error(decl->location(), "Function `{}` has non-void return type, and must return a value", decl->name());
-                    return;
-                }
+            if (block->children().empty()) {
+                if (ty->return_type()->is_void()) {
+                    block->add(new (mod) ReturnExpr(nullptr, {}));
+                } else {
+                    // For anything except the top-level function, if there is an expected
+                    // return value, there has to be one, otherwise it's an error.
+                    if (decl->name() != "main") {
+                        Error(decl->location(), "Function `{}` has non-void return type, and must return a value", decl->name());
+                        return;
+                    }
 
-                // For the top level function, a return value is be created if a valid
-                // one is not present.
-                auto inserted_return_value = new (mod) IntegerLiteral(0, {});
-                block->add(new (mod) ReturnExpr(inserted_return_value, {}));
+                    // For the top level function, a return value is be created if a valid
+                    // one is not present.
+                    auto inserted_return_value = new (mod) IntegerLiteral(0, {});
+                    block->add(new (mod) ReturnExpr(inserted_return_value, {}));
+                }
             }
 
             last = &block->children().back();
@@ -1572,7 +1576,7 @@ bool intc::Sema::Analyse(Type** type_ptr) {
                 Analyse(&param.type);
             }
 
-            /// If the function returns void, it must not be discardable.
+            /// If the function returns void, it must not be marked discardable.
             if (ty->return_type()->ok() and ty->return_type()->is_void()) {
                 if (ty->has_attr(FuncAttr::Discardable))
                     Error(type->location(), "Function returning void cannot be 'discardable'");
