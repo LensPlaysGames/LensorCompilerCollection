35fd342da76d4e2c7aff0373c8f54c028e1c38d4
Lens_r <lensplaysgames@gmail.com>
2023-11-05 13:27:07 -0800

    [ISel/x86_64] Store(Register, Local) pattern

--- include/lcc/codegen/isel_patterns_x86_64.hh
+++ include/lcc/codegen/isel_patterns_x86_64.hh
@@ -7,23 +7,29 @@
 namespace lcc {
 namespace isel {
 
+using OK = OperandKind;
+
+using store_reg_local = Pattern<
+    InstList<Inst<usz(MInst::Kind::Store), Operand<OK::Register, Register<0, 0>>, Operand<OK::Local, Local<>>>>,
+    InstList<Inst<usz(x86_64::Opcode::Move), Operand<OK::InputOperandReference, o<0>>, Operand<OK::InputOperandReference, o<1>>>>>;
 
 using ret = Pattern<
     InstList<Inst<usz(MInst::Kind::Return)>>,
-    InstList<Inst<usz(x86_64Opcode::Return)>>>;
+    InstList<Inst<usz(x86_64::Opcode::Return)>>>;
 
 // InputOperandReference operand with a value of o<0> means replace the
 // operand with whatever the zero-eth operand is in the input
 // instructions; in this case, the immediate operand of the gMIR return.
 using ret_imm = Pattern<
-    InstList<Inst<usz(MInst::Kind::Return), Operand<OperandKind::Immediate, Immediate<>>>>,
+    InstList<Inst<usz(MInst::Kind::Return), Operand<OK::Immediate, Immediate<>>>>,
     InstList<
-        Inst<usz(x86_64Opcode::Move), Operand<OperandKind::InputOperandReference, o<0>>, Operand<OperandKind::Register, Register<usz(lcc::x86_64::RegisterId::RETURN), 0>>>,
-        Inst<usz(x86_64Opcode::Return)>>>;
+        Inst<usz(x86_64::Opcode::Move), Operand<OK::InputOperandReference, o<0>>, Operand<OK::Register, Register<usz(lcc::x86_64::RegisterId::RETURN), 0>>>,
+        Inst<usz(x86_64::Opcode::Return)>>>;
 
 using x86_64PatternList = PatternList<
     ret,
-    ret_imm>;
+    ret_imm,
+    store_reg_local>;
 
 } // namespace isel
 } // namespace lcc
