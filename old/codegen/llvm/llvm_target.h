03d0ea6bc6848b53f86e81ae05a85385b49cd85c
Lens_r <lensplaysgames@gmail.com>
2023-11-20 16:34:33 -0800

    A New Era Begins
    So, that's gone! YEET
    
    Really, this code has been dead for a few months now. This is just
    when it has finally come time to part with it.
    
    Goodbye, sweet codebase. It was fun writing you, from scratch, in C :P

--- old/codegen/llvm/llvm_target.h
+++
@@ -1,9 +0,0 @@
-#ifndef INTERCEPT_LLVM_LLVM_TARGET_H
-#define INTERCEPT_LLVM_LLVM_TARGET_H
-
-#include <codegen/codegen_forward.h>
-
-/// Emit LLVM IR.
-void codegen_emit_llvm(CodegenContext* ctx);
-
-#endif // INTERCEPT_LLVM_LLVM_TARGET_H
--- old/codegen/x86_64/arch_x86_64_tgt_assembly.h
+++
@@ -1,9 +0,0 @@
-#ifndef ARCH_X86_64_TGT_ASSEMBLY_H
-#define ARCH_X86_64_TGT_ASSEMBLY_H
-
-#include <codegen/codegen_forward.h>
-
-/// Emits into `context->code` file.
-void emit_x86_64_assembly(CodegenContext *context, MIRFunctionVector machine_instructions);
-
-#endif /* ARCH_X86_64_TGT_ASSEMBLY_H */
