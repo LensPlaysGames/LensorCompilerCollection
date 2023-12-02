15ee6bd56bdc5de06280a679a49cdbe090987580
Lens_r <lensplaysgames@gmail.com>
2023-12-02 15:34:11 -0800

    [x86_64/GObj] Begin assembler

--- lib/lcc/codegen/x86_64/object.cc
+++ lib/lcc/codegen/x86_64/object.cc
@@ -1,9 +1,54 @@
 #include <lcc/codegen/generic_object.hh>
+#include <lcc/codegen/mir.hh>
 #include <lcc/codegen/x86_64/object.hh>
 #include <lcc/context.hh>
+#include <variant>
 
 namespace lcc {
 namespace x86_64 {
+
+// Must be MOperand* types
+template <typename Op1, typename Op2>
+bool is_two_operand(MInst& inst) {
+    // clang-format off
+    return inst.all_operands().size() == 2
+           and std::holds_alternative<Op1>(inst.get_operand(0))
+           and std::holds_alternative<Op2>(inst.get_operand(1));
+    // clang-format on
+}
+
+bool is_reg_imm(MInst& inst) {
+    return is_two_operand<MOperandRegister, MOperandImmediate>(inst);
+}
+bool is_reg_reg(MInst& inst) {
+    return is_two_operand<MOperandRegister, MOperandRegister>(inst);
+}
+bool is_reg_local(MInst& inst) {
+    return is_two_operand<MOperandRegister, MOperandLocal>(inst);
+}
+bool is_imm_imm(MInst& inst) {
+    return is_two_operand<MOperandImmediate, MOperandImmediate>(inst);
+}
+bool is_imm_reg(MInst& inst) {
+    return is_two_operand<MOperandImmediate, MOperandRegister>(inst);
+}
+bool is_imm_local(MInst& inst) {
+    return is_two_operand<MOperandImmediate, MOperandLocal>(inst);
+}
+bool is_local_imm(MInst& inst) {
+    return is_two_operand<MOperandLocal, MOperandImmediate>(inst);
+}
+bool is_local_reg(MInst& inst) {
+    return is_two_operand<MOperandLocal, MOperandRegister>(inst);
+}
+
+static void assemble(MFunction& func, Section& text) {
+    for (auto& block : func.blocks()) {
+        for (auto& inst : block.instructions()) {
+            // TODO: Assemble instruction
+        }
+    }
+}
 
 GenericObject emit_mcode_gobj(Module* module, const MachineDescription& desc, std::vector<MFunction>& mir) {
     GenericObject out{};
@@ -41,13 +86,13 @@ GenericObject emit_mcode_gobj(Module* module, const MachineDescription& desc, st
             out.symbols.push_back(sym);
         }
 
-        // TODO: Assemble function into machine code.
-
+        // Assemble function into machine code.
+        assemble(func, text);
     }
 
-    LCC_TODO("Actually assemble into machine code, populate symbols, etc");
+    // LCC_TODO("Actually assemble into machine code, populate symbols, etc");
 
-    return {};
+    return out;
 }
 
 } // namespace x86_64
