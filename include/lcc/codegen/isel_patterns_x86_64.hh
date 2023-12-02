ac27d7bb66f8d42023d9eac297960131c23ce6a0
Lens_r <lensplaysgames@gmail.com>
2023-12-02 14:59:58 -0800

    [x86_64/ISel] cmp imm imm

--- include/lcc/codegen/isel_patterns_x86_64.hh
+++ include/lcc/codegen/isel_patterns_x86_64.hh
@@ -219,71 +219,107 @@ using s_gt_eq_reg_imm = cmp_reg_imm<MInst::Kind::SGe, Opcode::SetByteIfEqualOrGr
 using eq_reg_imm = cmp_reg_imm<MInst::Kind::Eq, Opcode::SetByteIfEqual>;
 using ne_reg_imm = cmp_reg_imm<MInst::Kind::Ne, Opcode::SetByteIfNotEqual>;
 
+template <MInst::Kind kind, Opcode set_opcode>
+using cmp_imm_imm = Pattern<
+    InstList<Inst<Clobbers<>, usz(kind), Immediate<0, 0>, Immediate<0, 0>>>,
+    InstList<
+        // NOTE: GNU ordering of operands
+        Inst<Clobbers<>, usz(Opcode::Move), o<0>, v<0, 0>>,
+        Inst<Clobbers<>, usz(Opcode::Compare), o<1>, v<0, 0>>,
+        Inst<Clobbers<>, usz(Opcode::Move), Immediate<0, 0>, i<0>>,
+        Inst<Clobbers<c<0>>, usz(set_opcode), i<0>>>>;
+
+using u_lt_imm_imm = cmp_imm_imm<MInst::Kind::ULt, Opcode::SetByteIfLessUnsigned>;
+using s_lt_imm_imm = cmp_imm_imm<MInst::Kind::SLt, Opcode::SetByteIfLessSigned>;
+using u_lt_eq_imm_imm = cmp_imm_imm<MInst::Kind::ULe, Opcode::SetByteIfEqualOrLessUnsigned>;
+using s_lt_eq_imm_imm = cmp_imm_imm<MInst::Kind::SLe, Opcode::SetByteIfEqualOrLessSigned>;
+using u_gt_imm_imm = cmp_imm_imm<MInst::Kind::UGt, Opcode::SetByteIfGreaterUnsigned>;
+using s_gt_imm_imm = cmp_imm_imm<MInst::Kind::SGt, Opcode::SetByteIfGreaterSigned>;
+using u_gt_eq_imm_imm = cmp_imm_imm<MInst::Kind::UGe, Opcode::SetByteIfEqualOrGreaterUnsigned>;
+using s_gt_eq_imm_imm = cmp_imm_imm<MInst::Kind::SGe, Opcode::SetByteIfEqualOrGreaterSigned>;
+using eq_imm_imm = cmp_imm_imm<MInst::Kind::Eq, Opcode::SetByteIfEqual>;
+using ne_imm_imm = cmp_imm_imm<MInst::Kind::Ne, Opcode::SetByteIfNotEqual>;
+
 using z_ext_reg = Pattern<
     InstList<Inst<Clobbers<>, usz(MInst::Kind::ZExt), Register<0, 0>>>,
     InstList<Inst<Clobbers<>, usz(Opcode::MoveZeroExtended), o<0>, i<0>>>>;
 
-using x86_64PatternList = PatternList<
-    ret,
-    ret_imm,
-    ret_reg,
-    load_global,
-    load_local,
-    load_reg,
-    store_reg_local,
-    store_imm_local,
-    store_imm_reg,
-    store_reg_reg,
-    copy_reg,
-    copy_global,
-    copy_local,
-    copy_imm,
+using x86_64PatternList = PatternList <
+                          ret,
+      ret_imm,
+      ret_reg,
+      load_global,
+      load_local,
+      load_reg,
+      store_reg_local,
+      store_imm_local,
+      store_imm_reg,
+      store_reg_reg,
+      copy_reg,
+      copy_global,
+      copy_local,
+      copy_imm,
 
-    s_ext_reg,
-    z_ext_reg,
+      s_ext_reg,
+      z_ext_reg,
 
-    and_reg_reg,
-    and_reg_imm,
+      shl_reg_reg,
+      shr_reg_reg,
+      sar_reg_reg,
 
-    add_local_imm,
-    add_reg_reg,
-    add_imm_reg,
-    add_reg_imm,
+      and_reg_reg,
+      and_reg_imm,
 
-    mul_imm_reg,
-    mul_reg_imm,
+      add_local_imm,
+      add_reg_reg,
+      add_imm_reg,
+      add_reg_imm,
 
-    sub_reg_reg,
-    sub_reg_imm,
+      mul_imm_reg,
+      mul_reg_imm,
 
-    bitcast_imm,
+      sub_reg_reg,
+      sub_reg_imm,
 
-    simple_function_call,
-    simple_block_branch,
-    cond_branch_reg,
-    cond_branch_imm,
+      bitcast_imm,
 
-    u_lt_reg_reg,
-    s_lt_reg_reg,
-    u_lt_eq_reg_reg,
-    s_lt_eq_reg_reg,
-    u_gt_reg_reg,
-    s_gt_reg_reg,
-    u_gt_eq_reg_reg,
-    s_gt_eq_reg_reg,
-    eq_reg_reg,
-    ne_reg_reg,
+      simple_function_call,
+      simple_block_branch,
+      cond_branch_reg,
+      cond_branch_imm,
 
-    u_lt_reg_imm,
-    s_lt_reg_imm,
-    u_lt_eq_reg_imm,
-    s_lt_eq_reg_imm,
-    u_gt_reg_imm,
-    s_gt_reg_imm,
-    u_gt_eq_reg_imm,
-    s_gt_eq_reg_imm,
-    eq_reg_imm,
-    ne_reg_imm>;
+      u_lt_reg_reg,
+      s_lt_reg_reg,
+      u_lt_eq_reg_reg,
+      s_lt_eq_reg_reg,
+      u_gt_reg_reg,
+      s_gt_reg_reg,
+      u_gt_eq_reg_reg,
+      s_gt_eq_reg_reg,
+      eq_reg_reg,
+      ne_reg_reg,
+
+      u_lt_reg_imm,
+      s_lt_reg_imm,
+      u_lt_eq_reg_imm,
+      s_lt_eq_reg_imm,
+      u_gt_reg_imm,
+      s_gt_reg_imm,
+      u_gt_eq_reg_imm,
+      s_gt_eq_reg_imm,
+      eq_reg_imm,
+      ne_reg_imm,
+
+      u_lt_imm_imm,
+      s_lt_imm_imm,
+      u_lt_eq_imm_imm,
+      s_lt_eq_imm_imm,
+      u_gt_imm_imm,
+      s_gt_imm_imm,
+      u_gt_eq_imm_imm,
+      s_gt_eq_imm_imm,
+      eq_imm_imm,
+      ne_imm_imm>;
 
 } // namespace isel
 } // namespace lcc
