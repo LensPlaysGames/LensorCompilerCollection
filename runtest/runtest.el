7f78f658ca2891bcbbacfeaa3cbd7e2cdc39b13c
Lens_r <lensplaysgames@gmail.com>
2025-10-17 11:26:00 -0700

    [RunTest] It Works

--- runtest/runtest.el
+++ runtest/runtest.el
@@ -25,19 +25,27 @@
 
 ;;; Code:
 
+;; For parsing test declarations written in Org-mode.
+(require 'org-element)
+
 (defcustom
-  lcc-path "/home/lens_r/Programming/LensorCompilerCollection/bld/lcc"
+  lcc-path "/home/lens_r/Programming/play/LensorCompilerCollection/dbg/lcc"
   "The path to the LCC compiler on your system")
 
 (defvar run-test--completed-list ()
   "List of completed tests")
-(setq run-test--completed-list ())
+
+(defvar run-test--passed-list ()
+  "List of completed tests that passed their assertions")
 
 (defun run-test--completed (test-name)
   (memq test-name run-test--completed-list))
 
 (defun run-test--mark-completed (test-name)
   (push test-name run-test--completed-list))
+
+(defun run-test--mark-passed (test-name)
+  (push test-name run-test--passed-list))
 
 (defun run-test--execute (executable-filepath test-name expected-status expected-output)
   "Run executable"
@@ -54,13 +62,26 @@
    (lambda (p e)
      (when (eq 'exit (process-status p))
        ;; (message "    executed %s (%i)" (file-relative-name executable-filepath) (process-exit-status p))
+
+       ;; Once the program has exited, we no longer need it's executable...
        (delete-file executable-filepath)
+
+       ;; Compare program exit status to expected program exit status.
        (when (not (= (process-exit-status p) expected-status))
-         (message "    UNEXPECTED STATUS\n\tEXPECTED %i\n\tGOT %i" expected-status (process-exit-status p)))
+         (message "UNEXPECTED STATUS: %s\n\tEXPECTED %i\n\tGOT %i"
+                  test-name expected-status (process-exit-status p)))
+       ;; Compare program output to expected program output.
+       ;; TODO: Better visualisation of what's different, invisible characters, etc.
        (when (not (string-equal exe-output expected-output))
-         (message "    UNEXPECTED OUTPUT\n\tEXPECTED:\n'%s'\n\tGOT:\n'%s'" expected-output exe-output))
-       (when (and (= (process-exit-status p) expected-status) (string-equal exe-output expected-output))
-         (message "    PASSED"))
+         (message "UNEXPECTED OUTPUT: %s\n\tEXPECTED:\n'%s'\n\tGOT:\n'%s'"
+                  test-name expected-output exe-output))
+       ;; Record passed/failed, and print a pretty message.
+       (if (and (= (process-exit-status p) expected-status) (string-equal exe-output expected-output))
+           (progn
+             (run-test--mark-passed test-name)
+             (message "PASSED: %s" test-name))
+         (message "FAILED: %s" test-name))
+       ;; Mark test as completed
        (run-test--mark-completed test-name)))))
 
 (defun run-test--gcc (source-filepath test-name expected-status expected-output)
@@ -116,36 +137,134 @@
              (run-test--gcc output-filepath test-name expected-status expected-output))
          (error "LCC returned non-zero exit code (%i) %s" (process-exit-status p) lcc-output))))))
 
+(defun run-test--wait-for-test-completion (test-name)
+  "Sit until a test is registered as completed"
+  (while (not (run-test--completed test-name))
+      (sit-for 0.01)))
+
 (defun run-test--file (language test-name source-filepath expected-status expected-output)
   ""
-  (message "Running %s Test %s" language (file-relative-name source-filepath))
-  (let ((proc (run-test--lcc language source-filepath test-name expected-status expected-output)))
-    ;; Wait until the test has been marked as completed...
-    (while (not (run-test--completed test-name))
-      (sleep-for 0.05))))
+  ;; (message "Running %s Test %s (at %s)" language test-name (file-relative-name source-filepath))
+  (run-test--lcc language source-filepath test-name expected-status expected-output)
+  ;; Wait until the test has been marked as completed...
+  (run-test--wait-for-test-completion test-name))
 
 ;; (mapc
 ;;  (lambda (name) (run-test--file "glint" name 0 ""))
 ;;  (directory-files "./runtest/corpus/glint" t directory-files-no-dot-files-regexp))
 
+(defun run-test--parse-test-from-org (org-filepath)
+  "Returns a property list containing information about the parsed test, or nil
+ if parsing did not succeed.
+Properties:
+:name    Human-readable name
+:source  Input source code
+:status  Expected status from compiled+executed test
+:output  Expected output from compiled+executed test
+
+Additional properties are included, but not necessary:
+:path    Path to org-file containing test declaration"
+  (when (not (file-name-absolute-p org-filepath))
+    (error "Expected absolute filepath to parse org file from..."))
+
+  (defvar test ()
+    "A property list containing information about the parsed test.")
+  (setq test
+        '(:name "" :source nil :status nil :output nil :path org-filepath))
+
+  (with-current-buffer (find-file-noselect org-filepath)
+    (let ((parsetree (org-element-parse-buffer)))
+      (org-element-map
+          parsetree
+          t
+        (lambda (elem)
+          ;; (message "%s" (org-element-interpret-data elem))
+          ;; (message "Contents:-->%s<--" elem)
+          (when (eq (org-element-type elem) 'headline)
+            (setq test
+                  (plist-put
+                   test :name (org-element-interpret-data (org-element-property :title elem)))))
+          (when (string-equal (org-element-property :name elem) "source")
+            (setq test
+                  (plist-put
+                   test :source (org-element-property :value elem))))
+          (when (string-equal (org-element-property :name elem) "status")
+            (setq test
+                  (plist-put
+                   test :status (string-to-number (org-element-property :value elem)))))
+          (when (string-equal (org-element-property :name elem) "output")
+            (setq test
+                  (plist-put
+                   test :output (org-element-property :value elem))))))))
+  (let ((invalid-name (string-empty-p (plist-get test :name)))
+        (invalid-source (not (plist-get test :source)))
+        (invalid-status (or (not (plist-get test :status)) (not (integerp (plist-get test :status)))))
+        (invalid-output (not (plist-get test :output))))
+    (when invalid-name
+      (message "Error: Could not parse test at %s: no test name" org-filepath))
+    (when invalid-source
+      (message "Error: Could not parse test at %s: no test source" org-filepath))
+    (when invalid-status
+      (message "Error: Could not parse test at %s: no expected status" org-filepath))
+    (when invalid-output
+      (message "Error: Could not parse test at %s: no expected output" org-filepath))
+    (if (or
+         invalid-name
+         invalid-source
+         invalid-status
+         invalid-output)
+        nil
+      test)))
+
 (defun run-test--glint-tests ()
   ""
-;; TODO: Parse tests from file(s)
-  (message "Running Glint Tests")
-  (run-test--file
-   "glint"
-   "Top Level Implicit Integer Return"
-   "corpus/glint/toplevel_implicit_integer_return.txt"
-   69 "")
-  (run-test--file
-   "glint"
-   "Dynamic Array Operations"
-   "corpus/glint/dynamic_array_operations.g"
-   0 "4\n2\n0\n\n420\n"))
+  (message "Running Glint Tests...")
+  ;; For every .org file in corpus/glint/ directory, parse and run test.
+  (mapc
+   (lambda (org-filepath)
+     (let ((test (run-test--parse-test-from-org org-filepath))
+           (test-source-filepath (make-temp-file (expand-file-name "glinttest") nil ".g")))
+       (if (not test)
+           (message "Error: Failed to parse test from org file %s" org-filepath)
+         (progn
+           ;; Write test source to a temporary file, so that LCC can parse from it.
+           (with-current-buffer (find-file test-source-filepath)
+             (insert (plist-get test :source))
+             ;; Actually write the file to disk.
+             (save-buffer)
+             ;; Remove buffer from Emacs' buffer list. This is mainly so that, if you
+             ;; run this script from within a running Emacs, your buffer list doesn't
+             ;; get crowded with temporary files that have already been deleted.
+             (kill-buffer))
+
+           ;; Run the test
+           (run-test--file
+            "glint"
+            (plist-get test :name)
+            test-source-filepath
+            (plist-get test :status)
+            (plist-get test :output))
+
+           ;; Once the test has finished running, delete the generated test source.
+           (delete-file test-source-filepath)))))
+   (directory-files "corpus/glint/" t "\\.org\\'"))
+  (message "Ran %s Glint Tests: %s Passed"
+           (length run-test--completed-list)
+           (length run-test--passed-list)))
 
 (defun run-test--main ()
   ""
-  (run-test--glint-tests))
+  ;; Initialize state
+  (setq run-test--completed-list ())
+  (setq run-test--passed-list ())
+
+  ;; Turn off file backups (becomes a mess with all of the I/O that tests
+  ;; require)
+  (let ((backup-inhibited t))
+    (run-test--glint-tests)
+    (message "Ran %s Total Tests: %s Passed"
+             (length run-test--completed-list)
+             (length run-test--passed-list))))
 
 (run-test--main)
 
