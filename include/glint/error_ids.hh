68a94b763643ed371b9d4652fce0f4d832703764
Lens_r <lensplaysgames@gmail.com>
2026-01-04 20:47:55 -0800

    [Meta] Make it `constexpr`

--- include/glint/error_ids.hh
+++ include/glint/error_ids.hh
@@ -24,8 +24,27 @@ enum struct ErrorId : unsigned {
     COUNT
 };
 
-extern const std::array<std::pair<ErrorId, const char*>, +ErrorId::COUNT + 1>
-    error_id_strings;
+constexpr std::array<
+    std::pair<ErrorId, const char*>,
+    7>
+    error_id_strings{
+        std::pair{ErrorId::INVALID, "ICE: INVALID ERROR ID"},
+
+        {ErrorId::InvalidByteLiteral, "invalid-literal/byte"},
+        {ErrorId::InvalidIntegerLiteral, "invalid-literal/integer"},
+        {ErrorId::InvalidStringLiteral, "invalid-literal/string"},
+
+        {ErrorId::Expected, "expected"},
+
+        {ErrorId::Miscellaneous, "miscellaneous"},
+
+        {ErrorId::COUNT, "ICE: INVALID ERROR ID"}
+    };
+
+static_assert(
+    error_id_strings.size() == +ErrorId::COUNT + 1,
+    "Exhaustive handling of ErrorId"
+);
 
 } // namespace lcc::glint
 
