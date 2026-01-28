6470328295b29b6948796ce05c867f03850555b6
LensPlaysGames <lensplaysgames@gmail.com>
2026-01-28 15:31:37 -0800

    [Fractionals] Leading and trailing zeroes

--- inc/lcc/utils/fractionals.hh
+++ inc/lcc/utils/fractionals.hh
@@ -3,6 +3,8 @@
 
 #include <lcc/utils.hh>
 
+#include <fmt/format.h>
+
 namespace lcc {
 
 struct FixedPointNumber {
@@ -10,18 +12,69 @@ struct FixedPointNumber {
     u64 fractional{};
 };
 
+struct DecimalFraction {
+    u64 whole{};
+    uint leading_zeroes{};
+
+    explicit constexpr DecimalFraction(
+        u64 whole_,
+        uint leading_zeroes_
+    )
+        : whole(whole_),
+          leading_zeroes(leading_zeroes_) {
+        LCC_ASSERT(
+            leading_zeroes < 64,
+            "Obscene amount of leading zeroes..."
+        );
+    }
+};
+
+// @brief Convert a decimal representation of a fractional part of a
+// number into the binary fractional representation of that fractional
+// number.
+//
+// Basically, the input number gets treated as a whole representation of
+// the numbers following the decimal point, rather than the numbers preceding.
+//
 // Given "5" return binary fractional for "0.5", 0b0.10
 // Given "25" return binary fractional for "0.25", 0b0.01
+// Given "250" return binary fractional for "0.25", 0b0.01
 //
 // The output binary fractional number is in Q0.64 format.
-u64 whole_to_fractional(u64 whole);
+u64 whole_to_fractional(DecimalFraction whole);
 
 // Input fractional must be in Q0.64 fixed point format.
 //
-// Given "0b0.100..." return whole number 5
-// Given "0b0.010..." return whole number 25
-u64 fractional_to_whole(u64 fractional);
+// Given "0b0.100..." return whole number 5  (i.e. "0.5")
+// Given "0b0.010..." return whole number 25 (i.e. "0.25")
+DecimalFraction fractional_to_whole(u64 fractional);
 
 } // namespace lcc
+
+template <>
+struct fmt::formatter<lcc::DecimalFraction> : formatter<string_view> {
+    template <typename FormatContext>
+    auto format(const lcc::DecimalFraction& f, FormatContext& ctx) const {
+        return fmt::format_to(
+            ctx.out(),
+            "{}{}",
+            std::string(f.leading_zeroes, '0'),
+            f.whole
+        );
+    }
+};
+
+template <>
+struct fmt::formatter<lcc::FixedPointNumber> : formatter<string_view> {
+    template <typename FormatContext>
+    auto format(const lcc::FixedPointNumber& f, FormatContext& ctx) const {
+        return fmt::format_to(
+            ctx.out(),
+            "{}.{}",
+            f.whole,
+            lcc::fractional_to_whole(f.fractional)
+        );
+    }
+};
 
 #endif // LCC_FRACTIONALS_HH
--- lib/lcc/utils/fractionals.cc
+++ lib/lcc/utils/fractionals.cc
@@ -1,10 +1,16 @@
 #include <lcc/utils/fractionals.hh>
 
 namespace lcc {
-u64 whole_to_fractional(u64 whole) {
+u64 whole_to_fractional(DecimalFraction decimal_fraction) {
+    u64 whole = decimal_fraction.whole;
+
     u64 fractional{};
     constexpr decltype(whole) one = 1;
     constexpr decltype(whole) bitwidth = sizeof(fractional) * CHAR_BIT;
+
+    // Adjust whole to remove all trailing zeroes.
+    while (whole and whole % 10 == 0)
+        whole /= 10;
 
     // The decimal point in the input is placed just to the left of the
     // first *decimal* digit. To properly understand the scale of each digit,
@@ -19,11 +25,12 @@ u64 whole_to_fractional(u64 whole) {
         }
     }
 
+    for (uint i = 0; i < decimal_fraction.leading_zeroes; ++i)
+        divisor *= 10;
+
     // For every digit
     uint place{1};
     while (whole and place < bitwidth) {
-        u64 fractional_for_digit{};
-
         // If the input decimal number multiplied by 2 increases past the next
         // power of 10, then we know that we have *some value* over one half; this
         // means we are able to set the relevant binary bit (some negative power
@@ -33,11 +40,8 @@ u64 whole_to_fractional(u64 whole) {
         if (whole >= divisor) {
             whole -= divisor;
             u64 adjusted_bit = one << (bitwidth - place);
-            fractional_for_digit |= adjusted_bit;
+            fractional |= adjusted_bit;
         }
-
-        // Accumulate
-        fractional += fractional_for_digit;
 
         ++place;
     }
@@ -49,29 +53,24 @@ u64 whole_to_fractional(u64 whole) {
     return fractional;
 }
 
-u64 fractional_to_whole(u64 fractional) {
+DecimalFraction fractional_to_whole(u64 fractional) {
     u64 whole{};
-    u64 exponent{1};
+    uint exponent{1};
+
+    // TODO: Multiples of five may overflow quickly
+    // 2^64 - 1 = 18446744073709551615
+    // 5^27     =  7450580596923827000
 
     while (fractional) {
-        // Stop if times ten would overflow (precision error)
-        // We try not to do the divide unless we absolutely have to.
-        // A 64 bit number can represent at least 10^19, so if it's less than that
-        // it can't overflow when multiplying by 10...
-        if (
-            whole
-            and exponent > 18
-            and (whole > std::numeric_limits<u64>::max() / 10)
-        ) break;
-
-        whole *= 10;
-
-        // Get top bit of fractional
-        u64 top_bit = fractional >> ((sizeof(fractional) * CHAR_BIT) - 1);
+        // Get most significant bit of fractional
+        constexpr u64 bitwidth = (sizeof(fractional) * CHAR_BIT);
+        u64 top_bit = fractional >> (bitwidth - 1);
         // Adjust fractional
-        fractional <<= 1;
+        // NOTE: It is *meant* to "overflow"; we are *trying* to discard the most
+        // significant binary digit.
+        fractional *= 2;
 
-        // i := relevant_digit
+        // i := top_bit
         // n := exponent
         // i * 2^-n = x * 10^-n
         //     multiply by 10
@@ -83,19 +82,60 @@ u64 fractional_to_whole(u64 fractional) {
         //     simplify
         // x = 5i
         if (top_bit) {
+            // FIXME: It may be smarter to use a LUT; an array of powers of five up to
+            // exponent 27.
             for (
                 u64 exponent_application = 0;
                 exponent_application < exponent;
                 ++exponent_application
-            ) top_bit *= 5;
+            ) {
+                // Prevent overflow of top_bit via multiplication.
+                if (top_bit > std::numeric_limits<decltype(top_bit)>::max() / 5)
+                    break;
+                top_bit *= 5;
+            }
+
+            // Prevent overflow of whole via addition.
+            if (whole > std::numeric_limits<decltype(top_bit)>::max() - top_bit)
+                break;
 
             whole += top_bit;
         }
 
+        // Stop if times ten would overflow (precision error)
+        if (whole > std::numeric_limits<u64>::max() / 10)
+            break;
+        whole *= 10;
+
+        // Perfect conversion. Break here to ensure exponent is correct.
+        if (not fractional)
+            break;
+
         ++exponent;
     }
 
-    return whole;
+    // Adjust whole to remove all trailing zeroes.
+    while (whole and whole % 10 == 0)
+        whole /= 10;
+
+    // NOTE: We could also divide by 10 until we get zero and count how many
+    // times we had to do that.
+    uint scale{1};
+    {
+        decltype(whole) whole_copy{whole / 10};
+        while (whole_copy) {
+            ++scale;
+            whole_copy /= 10;
+        }
+    }
+    LCC_ASSERT(
+        scale <= exponent,
+        "Invalid scale (exponent: {}, scale: {})",
+        exponent,
+        scale
+    );
+    auto leading_zeroes = exponent - scale;
+    return DecimalFraction{whole, leading_zeroes};
 }
 
 } // namespace lcc
