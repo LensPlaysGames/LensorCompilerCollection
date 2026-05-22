57c3816a224e651d7f68ffe8516b0fbf255b704a
Lens_r <lensplaysgames@gmail.com>
2026-05-22 03:31:33 -0700

    [LanguageOfC] Minimal IRGen Framework

--- inc/language_c/irgen.hh
+++ inc/language_c/irgen.hh
@@ -2,14 +2,29 @@
 #define LANGUAGE_C_IRGEN_HH
 
 #include <lcc/context.hh>
+#include <lcc/core.hh>
+#include <lcc/ir/core.hh>
+#include <lcc/ir/module.hh>
+#include <lcc/ir/type.hh>
 
 #include <language_c/ast.hh>
 
 namespace lcc::language_c {
 
 class IRGen {
+    lcc::Context* context{};
+    lcc::Module* ir_module{};
+    std::unordered_map<Node*, lcc::Value*> generated_ir{};
+
+    void create_function(Declaration* d);
+
+    lcc::Type* convert(const Type* t);
 
 public:
+    IRGen(lcc::Context* context_)
+        : context(context_),
+          ir_module(new lcc::Module(context)) {}
+
     static auto Generate(Context*, TranslationUnit&) -> lcc::Module*;
 };
 
--- lib/language_c/irgen.cc
+++ lib/language_c/irgen.cc
@@ -1,13 +1,44 @@
 #include <language_c/irgen.hh>
 
 #include <lcc/context.hh>
+#include <lcc/core.hh>
+#include <lcc/ir/core.hh>
+#include <lcc/ir/module.hh>
+#include <lcc/ir/type.hh>
 
 #include <language_c/ast.hh>
 
 namespace lcc::language_c {
 
+lcc::Type* IRGen::convert(const Type* t) {
+    switch (t->kind()) {
+        case TypeKind::Int:
+        case TypeKind::Pointer:
+        case TypeKind::Function:
+        case TypeKind::Array: {
+            Diag::ICE("TODO: {} type irgen conversion", *t);
+        }
+        case TypeKind::Invalid:
+        case TypeKind::Count: break;
+    }
+    Diag::ICE("unreachable");
+}
+
+void IRGen::create_function(Declaration* d) {
+    generated_ir[d] = new (*ir_module) lcc::Function(
+        ir_module,
+        std::string(d->name()),
+        as<lcc::FunctionType>(convert(d->type())),
+        Linkage::Exported,
+        CallConv::C,
+        {}
+    );
+}
+
 auto IRGen::Generate(Context* context, TranslationUnit& tu) -> lcc::Module* {
-    return {};
+    IRGen irgen{context};
+
+    return irgen.ir_module;
 }
 
 } // namespace lcc::language_c
